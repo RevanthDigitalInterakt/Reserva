403da1c6ea004989bac9869942fb0148fc3f2b90
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-16 18:18:39 -0300

    Top bar added

--- src/modules/Home/pages/Home.tsx
+++ src/modules/Home/pages/Home.tsx
@@ -3,24 +3,57 @@ import { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-import { Typography, Box, HomeCard, BottomBar } from 'reserva-ui';
+import { Typography, Box, HomeCard, BottomBar, TopBar } from 'reserva-ui';
 import { ApplicationState } from '../../../store';
 import { loadRequest } from '../../../store/ducks/repositories/actions';
+import { RepositoriesState } from '../../../store/ducks/repositories/types';
 
 export const HomeScreen: React.FC<{
 	title: string;
 }> = ({ children, title }) => {
 	const dispatch = useDispatch();
-	const repositories = useSelector((state: ApplicationState) => state.repositories);
+	// const repositories = useSelector((state: ApplicationState) => state.repositories);
+
+	const { repositories } = useSelector((state: ApplicationState) => state);
 
 	useEffect(() => {
 		dispatch(loadRequest());
 	}, []);
 
 	return (
-		<SafeAreaView flex={1}>
-			<Box variant="container" alignItems="center" justifyContent="center">
-				<Typography variant="heading1">Lista de Repositórios</Typography>
+		<Box flex={1}>
+			<Box>
+				<TopBar
+					paddingX="xxxs"
+					bg="white"
+					boxShadow="topBarShadow"
+					leftButton={{
+						name: 'SideMenu',
+						size: 24,
+						onPress: () => {
+							Alert.alert('button left');
+						}
+					}}
+					rightButton1={{
+						name: 'Heart',
+						size: 24,
+						onPress: () => {
+							Alert.alert('button right 1');
+						}
+					}}
+					rightButton2={{
+						name: 'Handbag',
+						size: 24,
+						onPress: () => {
+							Alert.alert('button right 2');
+						}
+					}}
+					height={50}
+				/>
+			</Box>
+
+			<Box variant="container" alignItems="flex-start" justifyContent="center">
+				<Typography variant="descontoTag1">Lista de Repositórios</Typography>
 				{repositories.loading ? (
 					<Typography>Loading</Typography>
 				) : (
@@ -29,6 +62,6 @@ export const HomeScreen: React.FC<{
 					})
 				)}
 			</Box>
-		</SafeAreaView>
+		</Box>
 	);
 };
