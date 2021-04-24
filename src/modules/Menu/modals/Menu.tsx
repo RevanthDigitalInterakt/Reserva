ab72e2280af8571bda905b57d738e028009185bf
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-24 15:22:56 -0300

    Partial commit

--- src/modules/Menu/modals/Menu.tsx
+++ src/modules/Menu/modals/Menu.tsx
@@ -3,13 +3,26 @@ import * as React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Box, Divider, Icon, SearchBar, TextField, theme, Typography } from 'reserva-ui';
 import { TopBarMenu } from '../components/TopBarMenu';
+import * as Animatable from 'react-native-animatable';
+import { Alert, ScrollView, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 interface IBreadCumbs {
 	title: string;
 }
+
+interface IMenuSubItem {
+	title: string;
+	onPress?: Function;
+  highlight?: boolean;
+}
 interface IMenuItem {
 	title: string;
+	subItemList: IMenuSubItem[];
 	opened?: boolean;
+	onPress?: Function;
+	index?: number;
+  highlight?: boolean;
 }
 
 const Breadcumbs: React.FC<IBreadCumbs> = ({ title }) => {
@@ -25,20 +38,52 @@ const Breadcumbs: React.FC<IBreadCumbs> = ({ title }) => {
 	);
 };
 
-const MenuItem: React.FC<IMenuItem> = ({ title, opened }) => {
+const MenuSubItem: React.FC<IMenuSubItem> = ({ title, onPress, highlight }) => {
 	return (
-		<Box justifyContent="space-between" marginY="micro" flexDirection="row" marginX="xxs">
-			<Typography fontSize={13} fontFamily="nunitoBold" fontWeight="bold">
-				{title.toUpperCase()}
+		<Box
+			bg="backgroundMenuOpened"
+			justifyContent="space-between"
+			paddingY="micro"
+			flexDirection="row"
+			paddingX="xxs"
+		>
+			<Typography fontSize={13} fontFamily="nunitoRegular" fontWeight={highlight ? "bold" : "normal"}>
+				{ title }
 			</Typography>
-			<Box>
-				<Icon
-					style={{ transform: [ { rotate: opened ? '90deg' : '0deg' } ] }}
-					name="ChevronRight"
-					color="preto"
-					size={16}
-				/>
+		</Box>
+	);
+};
+
+const MenuItem: React.FC<IMenuItem> = ({ title, opened, onPress, index, subItemList, highlight }) => {
+	return (
+		<Box>
+      		<TouchableOpacity onPress={() => onPress(index)}>
+			<Box justifyContent="space-between" marginY="micro" flexDirection="row" marginX="xxs">
+		
+					<Typography color={highlight ? "vermelhoAlerta" :  "preto" } fontSize={13} fontFamily="nunitoBold" fontWeight="bold">
+						{title.toUpperCase()}
+					</Typography>
+			 
+				<Box>
+					<Icon
+						style={{ transform: [ { rotate: opened ? '90deg' : '0deg' } ] }}
+						name="ChevronRight"
+						color="preto"
+						size={16}
+					/>
+				</Box>
 			</Box>
+      </TouchableOpacity>
+			{opened && (
+        <>
+        <Divider variant="fullWidth" marginTop="micro" />
+				<Animatable.View animation="fadeIn">
+					{subItemList.map((item) => {
+						return <MenuSubItem highlight={item.highlight} title={item.title} onPress={() => {}} />;
+					})}
+				</Animatable.View>
+        </>
+			)}
 		</Box>
 	);
 };
@@ -46,16 +91,109 @@ const MenuItem: React.FC<IMenuItem> = ({ title, opened }) => {
 export const Menu: React.FC<{}> = () => {
 	const navigation = useNavigation();
 
+  const [mockSubItemList] = useState([
+    { title: 'Roupas', highlight: true },
+    { title: 'Ver tudo' },
+    { title: 'Camisetas' },
+    { title: 'Camisas' },
+    { title: 'Polos' },
+    { title: 'Casacos' },
+    { title: 'Bermudas' },
+    { title: 'Calças' },
+    { title: 'Cuecas' },
+    { title: 'Sungas' },
+  ])
+  const [mockItens, setMockItens] = useState([
+		{
+			title: 'Novidades',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+		{
+			title: 'Masculino',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+		{
+			title: 'Infantil',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+		{
+			title: 'Calçados',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+		{
+			title: 'Acessórios',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+		{
+			title: 'Crie sua Camiseta',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+    {
+			title: 'Parcerias',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+    {
+			title: 'Ofertas',
+			opened: false,
+			subItemList: mockSubItemList,
+      highlight: true
+		},
+    {
+			title: 'Sobre a Reserva',
+			opened: false,
+			subItemList: mockSubItemList
+		},
+	]);
+
+	const openMenuItem = (index: number) => {    
+    let itens: IMenuItem[] = JSON.parse(JSON.stringify(mockItens));
+
+    if (itens[index].opened) {
+      itens[index].opened = false;
+      setMockItens(itens);
+      return;
+    }
+
+    itens.forEach((menuItem, itemIndex) => {
+      if (menuItem.opened) {
+        menuItem.opened = false;
+      }
+    });
+
+    itens[index].opened = !itens[index].opened;
+
+    setMockItens(itens);
+  };
+
 	return (
 		<SafeAreaView style={{ backgroundColor: theme.colors.white, flex: 1 }}>
 			<Box flex={1} backgroundColor="backgroundMenu">
 				<TopBarMenu />
-				<Box paddingX="nano" paddingTop="micro">
-					<SearchBar height={36} placeholder="Buscar" />
-				</Box>
-				<Breadcumbs title="paginaInicial" />
-				<Divider variant="fullWidth" marginY="xxxs" />
-				<MenuItem title={'Novidades'} />
+				<ScrollView>
+					<Box paddingX="nano" paddingTop="micro">
+						<SearchBar height={36} placeholder="Buscar" />
+					</Box>
+					<Breadcumbs title="Página Inicial" />
+					<Divider variant="fullWidth" marginBottom="nano" marginTop="nano" />
+					{mockItens.map((item, index) => {
+						return (
+							<MenuItem 
+                highlight={item.highlight} 
+                subItemList={item.subItemList} 
+                onPress={openMenuItem} 
+                opened={item.opened} 
+                index={index} 
+                title={item.title} />
+						);
+					})}
+				</ScrollView>
 			</Box>
 		</SafeAreaView>
 	);
