6dcfec3518a8d14f5ec46d5f7473dc23d88dedef
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-25 10:51:03 -0300

    Partial commit

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,7 +1,9 @@
 export const images = {
-	topBannerMock1: require('./img/topBannerMock1.png'),
-	homeMock1: require('./img/homeMock1.png'),
-	homeMock2: require('./img/homeMock2.png'),
-	homeMock3: require('./img/homeMock3.png'),
-	homeMock4: require('./img/homeMock4.png')
+  topBannerMock1: require("./img/topBannerMock1.png"),
+  homeMock1: require("./img/homeMock1.png"),
+  homeMock2: require("./img/homeMock2.png"),
+  homeMock3: require("./img/homeMock3.png"),
+  homeMock4: require("./img/homeMock4.png"),
+  bannerOffer: require("./img/bannerOffer.png"),
+  bannerCatalog: require("./img/bannerCatalog.png"),
 };
