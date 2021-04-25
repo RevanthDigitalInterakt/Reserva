4cf9c9af96611be840e057dc22005ef4c43a7708
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-25 12:46:43 -0300

    Partial commit offer

---
+++ src/modules/Menu/components/TopBarDefaultBackButton.tsx
@@ -0,0 +1,42 @@
+import { useNavigation } from "@react-navigation/native";
+import * as React from "react";
+import { Alert, Platform } from "react-native";
+import { TopBar } from "reserva-ui";
+
+export const TopBarDefaultBackButton: React.FC<{
+  showShadow?: Boolean;
+}> = ({ showShadow = true }) => {
+  const navigation = useNavigation();
+
+  return (
+    <TopBar
+      paddingX="quarck"
+      bg="white"
+      style={{ elevation: 10 }}
+      boxShadow={showShadow && Platform.OS === "ios" ? "topBarShadow" : null}
+      leftButton={{
+        name: "ArrowBack",
+        size: 24,
+        onPress: () => {
+          navigation.goBack();
+        },
+      }}
+      rightButton1={{
+        name: "Heart",
+        size: 24,
+        onPress: () => {
+          Alert.alert("button right 1");
+        },
+      }}
+      rightButton2={{
+        name: "Handbag",
+        size: 24,
+        onPress: () => {
+          // Alert.alert('button right 2');
+          navigation.navigate("MyAddress");
+        },
+      }}
+      height={50}
+    />
+  );
+};
--- src/modules/ProductCatalog/pages/ProductCatalog.tsx
+++ src/modules/ProductCatalog/pages/ProductCatalog.tsx
@@ -2,14 +2,25 @@ import { useNavigation } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
 import * as React from "react";
 import { Component, useEffect } from "react";
+import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
-import { Typography, Box, Button, theme } from "reserva-ui";
+import {
+  Typography,
+  Box,
+  Button,
+  theme,
+  Image,
+  ProductListCard,
+} from "reserva-ui";
 import { backgroundColor } from "styled-system";
+import { images } from "../../../assets";
 import { RootStackParamList } from "../../../routes/StackNavigator";
 import { ApplicationState } from "../../../store";
 import { loadRequest } from "../../../store/ducks/repositories/actions";
+import { TopBarBackButton } from "../../Menu/components/TopBarBackButton";
 import { TopBarDefault } from "../../Menu/components/TopBarDefault";
+import { TopBarDefaultBackButton } from "../../Menu/components/TopBarDefaultBackButton";
 
 type Props = StackScreenProps<RootStackParamList, "ProductCatalog">;
 
@@ -31,28 +42,76 @@ export const ProductCatalog: React.FC<Props> = ({ route, navigation }) => {
       style={{ backgroundColor: safeArea ? theme.colors.white : null }}
       flex={1}
     >
-      <TopBarDefault />
-      <Box
-        bg="backgroundApp"
-        variant="container"
-        alignItems="flex-start"
-        justifyContent="center"
-      >
-        <Typography fontFamily="reservaSerifBold" fontSize={24}>
-          Lista de Repositórios
-        </Typography>
-        <Button
-          onPress={() => navigation.navigate("MyModal")}
-          title="Open Modal"
-        />
-        {repositories.loading ? (
-          <Typography>Loading</Typography>
-        ) : (
-          repositories.data.map((repository) => {
-            return <Typography>{repository.name}</Typography>;
-          })
-        )}
-      </Box>
+      {safeArea ? <TopBarDefaultBackButton /> : <TopBarDefault />}
+      <ScrollView>
+        <Box
+          bg="white"
+          variant="container"
+          alignItems="flex-start"
+          justifyContent="center"
+        >
+          <Box width={1 / 1}>
+            <Image
+              source={safeArea ? images.bannerCatalog : images.bannerOffer}
+              width={1 / 1}
+            />
+            <Box
+              p="micro"
+              flexDirection="row"
+              flex={1}
+              justifyContent="space-between"
+            >
+              <ProductListCard
+                currency="R$"
+                discountTag={18}
+                imageSource="https://via.placeholder.com/163x248"
+                productTitle="CAMISETA BÁSICA RESERVA"
+                installmentsNumber={3}
+                installmentsPrice={99.9}
+                price={345.0}
+                priceWithDiscount={297.0}
+                isFavorited={true}
+              />
+              <ProductListCard
+                currency="R$"
+                imageSource="https://via.placeholder.com/163x248"
+                productTitle="CAMISETA BÁSICA RESERVA"
+                installmentsNumber={3}
+                installmentsPrice={99.9}
+                price={345.0}
+                isFavorited={false}
+              />
+            </Box>
+            <Box
+              p="micro"
+              flexDirection="row"
+              flex={1}
+              justifyContent="space-between"
+            >
+              <ProductListCard
+                currency="R$"
+                discountTag={18}
+                imageSource="https://via.placeholder.com/163x248"
+                productTitle="CAMISETA BÁSICA RESERVA"
+                installmentsNumber={3}
+                installmentsPrice={99.9}
+                price={345.0}
+                priceWithDiscount={297.0}
+                isFavorited={true}
+              />
+              <ProductListCard
+                currency="R$"
+                imageSource="https://via.placeholder.com/163x248"
+                productTitle="CAMISETA BÁSICA RESERVA"
+                installmentsNumber={3}
+                installmentsPrice={99.9}
+                price={345.0}
+                isFavorited={false}
+              />
+            </Box>
+          </Box>
+        </Box>
+      </ScrollView>
     </DynamicComponent>
   );
 };
