0b7a0dbfec7579b95d0748900b57bfd925390e6c
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-26 16:26:08 -0300

    Spread fix

--- src/modules/Profile/pages/EditProfile.tsx
+++ src/modules/Profile/pages/EditProfile.tsx
@@ -1,55 +1,68 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Typography, Box, Button, Avatar, TextField, Icon } from 'reserva-ui';
-import { textAlign } from 'styled-system';
-import { ApplicationState } from '../../../store';
-import { loadRequest } from '../../../store/ducks/repositories/actions';
-import { TopBarBackButton } from '../../Menu/components/TopBarBackButton';
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Typography, Box, Button, Avatar, TextField, Icon } from "reserva-ui";
+import { textAlign } from "styled-system";
+import { ApplicationState } from "../../../store";
+import { loadRequest } from "../../../store/ducks/repositories/actions";
+import { TopBarBackButton } from "../../Menu/components/TopBarBackButton";
 
 export const EditProfile: React.FC<{
-	title: string;
+  title: string;
 }> = ({ children, title }) => {
-	const navigation = useNavigation();
-	const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
   const [data, setData] = useState({
-    name : 'João da Silva'
+    name: "João da Silva",
+    email: "João da Silva",
   });
 
+  useEffect(() => {
+    dispatch(loadRequest());
+  }, []);
 
-	useEffect(() => {
-		dispatch(loadRequest());
-	}, []);
+  return (
+    <Box flex={1}>
+      <TopBarBackButton />
 
-	return (
-		<Box flex={1}>
-			<TopBarBackButton />
-
-			<Box alignContent={"flex-start"} pt={'xs'} paddingX={'xxxs'}>
-        <Box alignItems={'center'}>
-          <Avatar buttonEdit={true} onPress={() => {
-            console.log('okx')
-          }}></Avatar>
+      <Box alignContent={"flex-start"} pt={"xs"} paddingX={"xxxs"}>
+        <Box alignItems={"center"}>
+          <Avatar
+            buttonEdit={true}
+            onPress={() => {
+              console.log("okx");
+            }}
+          ></Avatar>
         </Box>
 
-				<Box mt={'xxxs'}>
-          <Box mb={'nano'}>
-					  <TextField label={'Digite seu nome completo'} 
-              value={data.name} 
+        <Box mt={"xxxs"}>
+          <Box mb={"nano"}>
+            <TextField
+              label={"Digite seu nome completo"}
+              value={data.name}
               onChangeText={(text) => {
-                setData({
-                  ...data,
-                  name : text
-                })
+                console.log({ ...data, ...{ name: text } });
+                setData({ ...data, ...{ name: text } });
               }}
-              iconRight={<Box ml="nano"><Icon color="neutroFrio2" name="Check" size={25} /></Box>} />
+              iconRight={
+                <Box ml="nano">
+                  <Icon color="neutroFrio2" name="Check" size={25} />
+                </Box>
+              }
+            />
           </Box>
 
-
-					<TextField label={'Digite seu e-mail'} value={'joao@email.com'} />
-				</Box>
-			</Box>		
-		</Box>
-	);
+          <TextField
+            label={"Digite seu e-mail"}
+            value={data.email}
+            onChangeText={(text) => {
+              console.log({ ...data, ...{ email: text } });
+              setData({ ...data, ...{ email: text } });
+            }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
