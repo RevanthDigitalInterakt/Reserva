8d04ad82479026151b41e3b26f51821dd89473bc
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-28 16:47:00 -0300

    Fix image

--- src/modules/HelpCenter/PagesHelp/SizeGuide.tsx
+++ src/modules/HelpCenter/PagesHelp/SizeGuide.tsx
@@ -1,9 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { SafeAreaView, ScrollView, Image } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import { useDispatch } from "react-redux";
-import { Typography, Box, Button, TextField, Icon,  } from "reserva-ui";
+import { Typography, Box, Button, TextField, Icon, Image } from "reserva-ui";
 import { images } from "../../../assets";
 import { loadRequest } from "../../../store/ducks/repositories/actions";
 import { TopBarBackButton } from "../../Menu/components/TopBarBackButton";
@@ -11,7 +11,6 @@ import { TopBarBackButton } from "../../Menu/components/TopBarBackButton";
 export const SizeGuide: React.FC<{}> = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
- 
 
   useEffect(() => {
     dispatch(loadRequest());
@@ -27,17 +26,26 @@ export const SizeGuide: React.FC<{}> = () => {
 
       <ScrollView>
         <Box variant="container" flex={1} pt={"xs"} paddingX={"xxxs"}>
-          <Box mb={'micro'} alignSelf={"flex-start"}>
-            <Typography fontFamily="reservaSerifRegular" fontSize={20}>Guia de tamanhos</Typography>
+          <Box mb={"micro"} alignSelf={"flex-start"}>
+            <Typography fontFamily="reservaSerifRegular" fontSize={20}>
+              Guia de tamanhos
+            </Typography>
           </Box>
 
           <Box mt={"xxxs"}>
             <Box width={1 / 1}>
-              <Image source={images.helpSizeGuide1} width={1 / 1} resizeMode={'contain'} />
+              <Image
+                source={images.helpSizeGuide1}
+                height={291}
+                resizeMode={"contain"}
+              />
             </Box>
-
             <Box width={1 / 1}>
-              <Image source={images.helpSizeGuide2} width={1 / 1} />
+              <Image
+                source={images.helpSizeGuide2}
+                height={291}
+                resizeMode={"contain"}
+              />
             </Box>
           </Box>
         </Box>
