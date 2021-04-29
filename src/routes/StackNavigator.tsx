825c8cd85fd1172053d7ff70b66779ebd206e223
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-04-29 19:13:52 -0300

    remove duplicated pages

--- src/routes/StackNavigator.tsx
+++ src/routes/StackNavigator.tsx
@@ -1,49 +1,49 @@
 // In App.js in a new project
-import * as React from 'react';
+import * as React from "react";
 
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
-import { HomeScreen } from '../modules/Home/pages/Home';
-import { SearchScreen } from '../modules/Search/pages/Search';
-import AddressList from '../modules/Address/pages/AddressList';
-import NewAddress from '../modules/Address/pages/NewAddress';
-import { Tabs } from './BottomTabNavigator';
-import { Menu } from '../modules/Menu/modals/Menu';
+import { HomeScreen } from "../modules/Home/pages/Home";
+import { SearchScreen } from "../modules/Search/pages/Search";
+import AddressList from "../modules/Address/pages/AddressList";
+import NewAddress from "../modules/Address/pages/NewAddress";
+import { Tabs } from "./BottomTabNavigator";
+import { Menu } from "../modules/Menu/modals/Menu";
 
-import { horizontalAnimationBackwards } from './utils/utils';
-import { ExampleScreen } from '../modules/Example/pages/Example';
-import { ProductCatalog } from '../modules/ProductCatalog/pages/ProductCatalog';
-import { RegisterSuccess } from '../modules/Register/pages/RegisterSuccess';
-import { LoginScreen } from '../modules/Login/pages/Login';
-import { BagScreen } from '../modules/Checkout/pages/Bag';
-import { DeliveryScreen } from '../modules/Checkout/pages/Delivery';
-import { GiftVoucherScreen } from '../modules/Checkout/pages/GiftVoucher';
-import { StackScreenProps } from '@react-navigation/stack';
-import OrderList from '../modules/Order/pages/OrderList';
-import OrderDetail from '../modules/Order/pages/OrderDetail';
-import OrderCancel from '../modules/Order/pages/OrderCancel';
-import { Register } from '../modules/Register/pages/Register';
-import { ForgotEmail } from '../modules/Forgot/pages/ForgotEmail';
-import { ForgotEmailSuccess } from '../modules/Forgot/pages/ForgotEmailSuccess';
-import { ForgotPassword } from '../modules/Forgot/pages/ForgotPassword';
-import { ForgotNewPassword } from '../modules/Forgot/pages/ForgotNewPassword';
-import { ProductDetail } from '../modules/ProductDetail/pages/ProductDetail';
+import { horizontalAnimationBackwards } from "./utils/utils";
+import { ExampleScreen } from "../modules/Example/pages/Example";
+import { ProductCatalog } from "../modules/ProductCatalog/pages/ProductCatalog";
+import { RegisterSuccess } from "../modules/Register/pages/RegisterSuccess";
+import { LoginScreen } from "../modules/Login/pages/Login";
+import { BagScreen } from "../modules/Checkout/pages/Bag";
+import { DeliveryScreen } from "../modules/Checkout/pages/Delivery";
+import { GiftVoucherScreen } from "../modules/Checkout/pages/GiftVoucher";
+import { StackScreenProps } from "@react-navigation/stack";
+import OrderList from "../modules/Order/pages/OrderList";
+import OrderDetail from "../modules/Order/pages/OrderDetail";
+import OrderCancel from "../modules/Order/pages/OrderCancel";
+import { Register } from "../modules/Register/pages/Register";
+import { ForgotEmail } from "../modules/Forgot/pages/ForgotEmail";
+import { ForgotEmailSuccess } from "../modules/Forgot/pages/ForgotEmailSuccess";
+import { ForgotPassword } from "../modules/Forgot/pages/ForgotPassword";
+import { ForgotNewPassword } from "../modules/Forgot/pages/ForgotNewPassword";
+import { ProductDetail } from "../modules/ProductDetail/pages/ProductDetail";
 
 //profile
-import { EditProfile } from '../modules/Profile/pages/EditProfile';
-import { EditPassword } from '../modules/Profile/pages/EditPassword';
-import { NotificationProfile } from '../modules/Profile/pages/NotificationProfile';
-import { MyCards } from '../modules/Profile/pages/MyCards';
-import { NewCard } from '../modules/Profile/pages/NewCard';
+import { EditProfile } from "../modules/Profile/pages/EditProfile";
+import { EditPassword } from "../modules/Profile/pages/EditPassword";
+import { NotificationProfile } from "../modules/Profile/pages/NotificationProfile";
+import { MyCards } from "../modules/Profile/pages/MyCards";
+import { NewCard } from "../modules/Profile/pages/NewCard";
 //--
 
-import { HelpCenter } from '../modules/HelpCenter/pages/HelpCenter';
-import { SizeGuide } from '../modules/HelpCenter/PagesHelp/SizeGuide';
-import { ClothingCare } from '../modules/HelpCenter/PagesHelp/ClothingCare';
+import { HelpCenter } from "../modules/HelpCenter/pages/HelpCenter";
+import { SizeGuide } from "../modules/HelpCenter/PagesHelp/SizeGuide";
+import { ClothingCare } from "../modules/HelpCenter/PagesHelp/ClothingCare";
 
-import { ContactUs } from '../modules/HelpCenter/PagesHelp/ContactUs';
-import { Exchanges } from '../modules/HelpCenter/PagesHelp/Exchanges';
-import { WhatsappsHelp } from '../modules/HelpCenter/PagesHelp/WhatsappsHelp';
+import { ContactUs } from "../modules/HelpCenter/PagesHelp/ContactUs";
+import { Exchanges } from "../modules/HelpCenter/PagesHelp/Exchanges";
+import { WhatsappsHelp } from "../modules/HelpCenter/PagesHelp/WhatsappsHelp";
 
 export type RootStackParamList = {
   ProductCatalog: { safeArea: boolean; search: boolean };
@@ -97,15 +97,13 @@ const MainStackScreen = () => {
       <MainStack.Screen name="OrderDetail" component={OrderDetail} />
       <MainStack.Screen name="OrderCancel" component={OrderCancel} />
 
-      <MainStack.Screen name="EditProfile" component={EditProfile} />
-      <MainStack.Screen name="EditPassword" component={EditPassword} />
-
       <MainStack.Screen name="HelpCenter" component={HelpCenter} />
       <MainStack.Screen name="SizeGuide" component={SizeGuide} />
       <MainStack.Screen name="ClothingCare" component={ClothingCare} />
       <MainStack.Screen name="ContactUs" component={ContactUs} />
       <MainStack.Screen name="Exchanges" component={Exchanges} />
       <MainStack.Screen name="WhatsappsHelp" component={WhatsappsHelp} />
+
       <MainStack.Screen name="EditProfile" component={EditProfile} />
       <MainStack.Screen name="EditPassword" component={EditPassword} />
       <MainStack.Screen
