16892d8a4baaf22712774dfe3edc889411455fe8
tiago.rodrigues <tiago.rodrigues@globalsys.com.br>
2021-05-25 16:52:20 -0300

    Validação adicionada

--- src/modules/Address/pages/NewAddress.tsx
+++ src/modules/Address/pages/NewAddress.tsx
@@ -12,7 +12,22 @@ import {
 } from 'react-native-masked-text';
 import { loadAddress, createAddress, createDefaultAddress, updateAddress } from "../../../store/ducks/address/actions";
 import { ApplicationState } from "../../../store";
+import { useFormikContext } from 'formik';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
+interface IAdress {
+  postalCode: string;
+  state: string;
+  city: string;
+  number: string;
+  complement: string;
+  district: string;
+  street: string;
+  recipientName?: string;
+  phoneNumber?: string;
+  sendMessage?: string;
+}
 type Props = StackScreenProps<RootStackParamList, 'NewAddress'>;
 
 export const NewAddress: React.FC<Props> = ({ route }) => {
@@ -20,38 +35,69 @@ export const NewAddress: React.FC<Props> = ({ route }) => {
   const navigation = useNavigation();
   const scrollViewRef = useRef<ScrollView>(null);
   const { edit, editAddress, } = route?.params;
-  const [addressId, setAddressId] = React.useState('');
+  const [addressId, setAddressId] = React.useState(edit ? editAddress.id : '');
   const [toggleActivated, setToggleActivated] = React.useState(false);
-  const [postalCode, setPostalCode] = useState('');
-  const [state, setState] = useState('ES');
-  const [city, setCity] = useState('Vitoria');
-  const [number, setNumber] = useState('123');
-  const [complement, setComplement] = useState('Em frente a oficina');
-  const [district, setDistrict] = useState('Camburi');
-  const [street, setStreet] = useState('Camburi');
-  const [recipientName, setRecipientName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [sendMessage, setSendMessage] = useState('');
+
   const { isCheckout } = route.params;
   const {
     address: { data: addresses, loading, error, defaultAddress },
 
   } = useSelector((state: ApplicationState) => state);
 
+  const [initialValues, setInitialValues] = useState<IAdress>({
+    postalCode: edit ? editAddress.postalCode : '11111',
+    state: edit ? editAddress.state : 'ES',
+    city: edit ? editAddress.city : 'VITORIA',
+    number: edit ? editAddress.numberAndComplement[0] : '370',
+    complement: edit ? editAddress.numberAndComplement[1] : 'Em frente a oficina',
+    district: edit ? editAddress.district : 'Vista da Penha',
+    street: edit ? editAddress.street : 'Rua Mercurio',
+    recipientName: edit ? editAddress.firstName : '',
+    phoneNumber: edit ? editAddress.phoneNumber : '',
+    sendMessage: edit ? editAddress.jobTitle : '',
+  })
+
+  const validation = Yup.object().shape({
+    postalCode: Yup.string().required('Informe um CEP')
+      .matches(/^(?=.{9,})/, { message: 'CEP não é valido' }),
+    state: Yup.string().required('Informe um Estado'),
+    city: Yup.string().required('Informe uma Cidade'),
+    number: Yup.string().required('Informe um número'),
+    district: Yup.string().required('Informe um bairro'),
+    street: Yup.string().required('Informe um endereço'),
+  })
+
   useEffect(() => {
     if (edit) {
+      console.log('editAddress', editAddress)
       setAddressId(editAddress.id);
-      setPostalCode(editAddress.postalCode);
-      setState(editAddress.state);
-      setCity(editAddress.city);
-      setStreet(editAddress.street);
-      setDistrict(editAddress.district);
-      setNumber(editAddress.numberAndComplement[0]);
-      setComplement(editAddress.numberAndComplement[1]);
+      setInitialValues({
+        postalCode: editAddress.postalCode,
+        state: editAddress.state,
+        city: editAddress.city,
+        number: editAddress.numberAndComplement[0],
+        complement: editAddress.numberAndComplement[1],
+        district: editAddress.district,
+        street: editAddress.street,
+        recipientName: editAddress.firstName,
+        phoneNumber: editAddress.phoneNumber,
+        sendMessage: editAddress.jobTitle,
+      });
     }
   }, [edit])
 
-  const addNewAddress = () => {
+  const addNewAddress = (
+    city: string,
+    complement: string,
+    district: string,
+    number: string,
+    postalCode: string,
+    state: string,
+    street: string,
+    phoneNumber: string,
+    recipientName: string,
+    sendMessage: string
+  ) => {
     if (edit) {
       dispatch(updateAddress(
         {
@@ -116,110 +162,143 @@ export const NewAddress: React.FC<Props> = ({ route }) => {
                   Receba em casa ou no endereço de sua preferência
                 </Typography>
               </Box>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validation}
+                onSubmit={(values) => {
+                  const { city, complement, district, number, postalCode, state, street, phoneNumber, recipientName, sendMessage } = values;
+                  addNewAddress(city, complement, district, number, postalCode, state, street, phoneNumber, recipientName, sendMessage)
+                  console.log('sucesso', values)
+                }}
+              >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
+                  <>
+                    <InputOption
+                      placeholder={'Digite seu CEP'}
+                      maskType={'zip-code'}
+                      value={values.postalCode}
+                      onChangeText={handleChange('postalCode')}
+                      touched={touched.postalCode}
+                      error={errors.postalCode && touched.postalCode ? `${errors.postalCode}` : null}
+                    />
 
-              <InputOption
-                placeholder={'Digite seu CEP'}
-                maskType={'zip-code'}
-                value={postalCode}
-                onChangeText={(value) => { setPostalCode(value) }}
-              />
+                    <Box flexDirection={'row'} justifyContent="space-between">
+                      <Box flex={1} marginRight={'micro'}>
+                        <InputOption
+                          placeholder={'Digite seu estado'}
+                          value={values.state}
+                          onChangeText={handleChange('state')}
+                          touched={touched.state}
+                          error={errors.state && touched.state ? `${errors.state}` : null}
+                        />
+                      </Box>
 
-              <Box flexDirection={'row'} justifyContent="space-between">
-                <Box flex={1} marginRight={'micro'}>
-                  <InputOption
-                    placeholder={'Digite seu estado'}
-                    value={state}
-                    onChangeText={(value) => { setState(value) }}
-                  />
-                </Box>
+                      <Box flex={1}>
+                        <InputOption
+                          placeholder={'Digite sua cidade'}
+                          value={values.city}
+                          onChangeText={handleChange('city')}
+                          touched={touched.city}
+                          error={errors.city && touched.city ? `${errors.city}` : null}
+                        />
+                      </Box>
+                    </Box>
 
-                <Box flex={1}>
-                  <InputOption
-                    placeholder={'Digite sua cidade'}
-                    value={city}
-                    onChangeText={(value) => { setCity(value) }}
-                  />
-                </Box>
-              </Box>
+                    <InputOption
+                      placeholder={'Endereço'}
+                      value={values.street}
+                      onChangeText={handleChange('street')}
+                      touched={touched.street}
+                      error={errors.street && touched.street ? `${errors.street}` : null}
+                    />
 
-              <InputOption
-                placeholder={'Endereço'}
-                value={street}
-                onChangeText={(value) => { setStreet(value) }}
-              />
+                    <Box flexDirection={'row'} justifyContent="space-between">
+                      <Box flex={1} marginRight={'micro'}>
+                        <InputOption
+                          placeholder={'Digite seu bairro'}
+                          value={values.district}
+                          onChangeText={handleChange('district')}
+                          touched={touched.district}
+                          error={errors.district && touched.district ? `${errors.district}` : null}
+                        />
+                      </Box>
 
-              <Box flexDirection={'row'} justifyContent="space-between">
-                <Box flex={1} marginRight={'micro'}>
-                  <InputOption
-                    placeholder={'Digite seu bairro'}
-                    value={district}
-                    onChangeText={(value) => { setDistrict(value) }}
-                  />
-                </Box>
+                      <Box flex={1}>
+                        <InputOption placeholder={'Número'}
+                          value={values.number}
+                          onChangeText={handleChange('number')}
+                          touched={touched.number}
+                          error={errors.number && touched.number ? `${errors.number}` : null}
+                        />
+                      </Box>
+                    </Box>
 
-                <Box flex={1}>
-                  <InputOption placeholder={'Número'}
-                    value={number}
-                    onChangeText={(value) => { setNumber(value) }}
-                  />
-                </Box>
-              </Box>
+                    <InputOption
+                      placeholder={'Complemento'}
+                      value={values.complement}
+                      onChangeText={handleChange('complement')}
+                      touched={touched.complement}
+                      error={errors.complement && touched.complement ? `${errors.complement}` : null}
+                    />
 
-              <InputOption
-                placeholder={'Complemento'}
-                value={complement}
-                onChangeText={(value) => { setComplement(value) }}
-              />
+                    <Box mt="xs" mb="xxxs">
+                      <Toggle
+                        label="A entrega é para presente"
+                        color="preto"
+                        thumbColor="vermelhoAlerta"
+                        value={toggleActivated}
+                        onValueChange={() => {
+                          setToggleActivated(!toggleActivated);
+                          scrollViewRef.current &&
+                            scrollViewRef.current.scrollToEnd({ animated: true });
+                        }}
+                      />
+                    </Box>
 
-              <Box mt="xs" mb="xxxs">
-                <Toggle
-                  label="A entrega é para presente"
-                  color="preto"
-                  thumbColor="vermelhoAlerta"
-                  value={toggleActivated}
-                  onValueChange={() => {
-                    setToggleActivated(!toggleActivated);
-                    scrollViewRef.current &&
-                      scrollViewRef.current.scrollToEnd({ animated: true });
-                  }}
-                />
-              </Box>
+                    {toggleActivated && (
+                      <Box mb={'sm'}>
+                        <InputOption
+                          placeholder={'Nome do destinatário'}
+                          value={values.recipientName}
+                          onChangeText={handleChange('recipientName')}
+                          touched={touched.recipientName}
+                          error={errors.recipientName && touched.recipientName ? `${errors.recipientName}` : null}
+                        />
 
-              {toggleActivated && (
-                <Box mb={'sm'}>
-                  <InputOption
-                    placeholder={'Nome do destinatário'}
-                    value={recipientName}
-                    onChangeText={(value) => { setRecipientName(value) }}
-                  />
+                        <InputOption
+                          maskType={'cel-phone'}
+                          placeholder={'Telefone para contato'}
+                          value={values.phoneNumber}
+                          onChangeText={handleChange('phoneNumber')}
+                          touched={touched.phoneNumber}
+                          error={errors.phoneNumber && touched.phoneNumber ? `${errors.phoneNumber}` : null}
+                        />
 
-                  <InputOption
-                    maskType={'cel-phone'}
-                    placeholder={'Telefone para contato'}
-                    value={phoneNumber}
-                    onChangeText={(value) => { setPhoneNumber(value) }}
-                  />
+                        <InputOption
+                          height={135}
+                          textAlignVertical={'top'}
+                          placeholder={'Deseja enviar algum recado junto?'}
+                          value={values.sendMessage}
+                          onChangeText={handleChange('sendMessage')}
+                          touched={touched.sendMessage}
+                          error={errors.sendMessage && touched.sendMessage ? `${errors.sendMessage}` : null}
+                        />
+                      </Box>
+                    )}
 
-                  <InputOption
-                    height={135}
-                    textAlignVertical={'top'}
-                    placeholder={'Deseja enviar algum recado junto?'}
-                    value={sendMessage}
-                    onChangeText={(value) => { setSendMessage(value) }}
-                  />
-                </Box>
-              )}
-
-              {!isCheckout && (
-                <Button
-                  disabled={loading}
-                  width="200px"
-                  mt={'xs'}
-                  onPress={addNewAddress}
-                  title={'SALVAR ALTERAÇÕES'}
-                  variant="primarioEstreitoOutline"
-                />
-              )}
+                    {!isCheckout && (
+                      <Button
+                        disabled={loading}
+                        width="200px"
+                        mt={'xs'}
+                        onPress={handleSubmit}
+                        title={'SALVAR ALTERAÇÕES'}
+                        variant="primarioEstreitoOutline"
+                      />
+                    )}
+                  </>
+                )}
+              </Formik>
             </Box>
           </Box>
         </ScrollView>
@@ -245,6 +324,8 @@ interface IInputOption {
   maskOptions?: TextInputMaskOptionProp;
   value?: string;
   height?: number;
+  error?: any;
+  touched?: any;
   textAlignVertical?: 'auto' | 'top' | 'bottom' | 'center' | undefined;
   onChangeText?: (value: string) => void;
 }
@@ -255,6 +336,8 @@ const InputOption = ({
   maskOptions,
   value,
   height,
+  error,
+  touched,
   textAlignVertical,
   onChangeText,
 }: IInputOption) => {
@@ -270,6 +353,8 @@ const InputOption = ({
           onChangeText={onChangeText}
           placeholder={placeholder}
           value={value}
+          touched={touched}
+          error={error}
         />
       </Box>
     </>
