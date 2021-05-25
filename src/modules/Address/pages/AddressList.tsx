3fbe118616c91ff8c9e52f30a190cc48848252d9
Erasmo Bellumat <lbj.erasmo@gmail.com>
2021-05-25 10:51:54 -0300

    Open Physical Address List

--- src/modules/Address/pages/AddressList.tsx
+++ src/modules/Address/pages/AddressList.tsx
@@ -1,32 +1,37 @@
-import * as React from 'react';
-import { SafeAreaView, ScrollView, FlatList } from 'react-native';
+import * as React from "react";
+import { SafeAreaView, ScrollView, FlatList } from "react-native";
 
-import { Typography, Box, Button, Alert } from 'reserva-ui';
+import { Typography, Box, Button, Alert } from "reserva-ui";
 // import AddressSelector, { Address } from '../Components/AddressSelector';
-import AddressSelector from '../Components/AddressSelector';
-import { TopBarBackButton } from '../../Menu/components/TopBarBackButton';
-import { useNavigation } from '@react-navigation/core';
-import { RootStackParamList } from '../../../routes/StackNavigator';
-import { StackScreenProps } from '@react-navigation/stack';
+import AddressSelector from "../Components/AddressSelector";
+import { TopBarBackButton } from "../../Menu/components/TopBarBackButton";
+import { useNavigation } from "@react-navigation/core";
+import { RootStackParamList } from "../../../routes/StackNavigator";
+import { StackScreenProps } from "@react-navigation/stack";
 import { useSelector, useDispatch } from "react-redux";
 import { ApplicationState } from "../../../store";
-import { loadAddress, deleteAddress, createDefaultAddress, deleteDefautAddress } from "../../../store/ducks/address/actions";
-import { Address } from '../../../store/ducks/address/types';
+import {
+  loadAddress,
+  deleteAddress,
+  createDefaultAddress,
+  deleteDefautAddress,
+} from "../../../store/ducks/address/actions";
+import { Address } from "../../../store/ducks/address/types";
 
-type Props = StackScreenProps<RootStackParamList, 'AddressList'>;
+type Props = StackScreenProps<RootStackParamList, "AddressList">;
 
 const AddressList: React.FC<Props> = ({ route }) => {
   const dispatch = useDispatch();
   const {
-    address: { data: addresses, loading, error, defaultAddress }
+    address: { data: addresses, loading, error, defaultAddress },
   } = useSelector((state: ApplicationState) => state);
 
   const navigation = useNavigation();
   const [deleteModal, setDeleteModal] = React.useState(false);
-  const [addressId, setAddressId] = React.useState('');
+  const [addressId, setAddressId] = React.useState("");
   const [successModal, setSuccessModal] = React.useState(false);
   const [selected, setSelected] = React.useState(false);
-  const [idAddress, setIdAddress] = React.useState<string | undefined>('');
+  const [idAddress, setIdAddress] = React.useState<string | undefined>("");
   const modalRef = React.useRef(false);
   const { isCheckout } = route.params;
 
@@ -34,20 +39,30 @@ const AddressList: React.FC<Props> = ({ route }) => {
     dispatch(loadAddress());
   }, []);
 
-
   React.useEffect(() => {
     if (defaultAddress) {
-      setIdAddress(defaultAddress?.address?.id)
+      setIdAddress(defaultAddress?.address?.id);
     } else {
-      setIdAddress('')
+      setIdAddress("");
     }
   }, [defaultAddress]);
   //verifica se tem o primeiro endereço, se tiver, então salva como padrão
   React.useEffect(() => {
     if (addresses?.length === 1) {
-      const { address1, address2, address3, city, postalCode, state, firstName, phoneNumber, jobTitle, id } = addresses[0].address
-      dispatch(createDefaultAddress(
-        {
+      const {
+        address1,
+        address2,
+        address3,
+        city,
+        postalCode,
+        state,
+        firstName,
+        phoneNumber,
+        jobTitle,
+        id,
+      } = addresses[0].address;
+      dispatch(
+        createDefaultAddress({
           address: {
             country: "BR",
             address3: address3,
@@ -59,12 +74,12 @@ const AddressList: React.FC<Props> = ({ route }) => {
             firstName: firstName,
             phoneNumber: phoneNumber,
             jobTitle: jobTitle,
-            id: id
-          }
-        }
-      ))
+            id: id,
+          },
+        })
+      );
     }
-  }, [addresses])
+  }, [addresses]);
 
   return (
     <>
@@ -73,15 +88,15 @@ const AddressList: React.FC<Props> = ({ route }) => {
           modalRef.current && setSuccessModal(true);
         }}
         isVisible={deleteModal}
-        title={'Excluir endereço'}
-        subtitle={'Tem certeza que deseja excluir o endereço salvo?'}
-        confirmText={'SIM'}
-        cancelText={'NÃO'}
+        title={"Excluir endereço"}
+        subtitle={"Tem certeza que deseja excluir o endereço salvo?"}
+        confirmText={"SIM"}
+        cancelText={"NÃO"}
         onConfirm={() => {
           modalRef.current = true;
-          dispatch(deleteAddress(addressId))
+          dispatch(deleteAddress(addressId));
           if (addressId === defaultAddress?.address?.id) {
-            dispatch(deleteDefautAddress())
+            dispatch(deleteDefautAddress());
           }
 
           setDeleteModal(false);
@@ -93,11 +108,11 @@ const AddressList: React.FC<Props> = ({ route }) => {
           setDeleteModal(false);
         }}
       />
-      {error ?
+      {error ? (
         <Alert
           isVisible={successModal}
-          title={"Não foi possível exluir o endereço"}
-          confirmText={'OK'}
+          title={"Não foi possível excluir o endereço"}
+          confirmText={"OK"}
           onConfirm={() => {
             setSuccessModal(false);
           }}
@@ -105,11 +120,11 @@ const AddressList: React.FC<Props> = ({ route }) => {
             setDeleteModal(false);
           }}
         />
-        :
+      ) : (
         <Alert
           isVisible={successModal}
           title={"Seu endereço foi excluido com sucesso."}
-          confirmText={'OK'}
+          confirmText={"OK"}
           onConfirm={() => {
             setSuccessModal(false);
           }}
@@ -117,7 +132,7 @@ const AddressList: React.FC<Props> = ({ route }) => {
             setDeleteModal(false);
           }}
         />
-      }
+      )}
 
       <SafeAreaView flex={1} backgroundColor="white">
         <TopBarBackButton
@@ -127,64 +142,78 @@ const AddressList: React.FC<Props> = ({ route }) => {
         />
 
         <Box
-          overflow={'hidden'}
-          height={'80%'}
+          overflow={"hidden"}
+          height={"80%"}
           paddingHorizontal={20}
           justifyContent="flex-start"
-          pt={'md'}
+          pt={"md"}
         >
-          <Box alignSelf={'flex-start'} mb={'xxxs'}>
+          <Box alignSelf={"flex-start"} mb={"xxxs"}>
             <Typography variant="tituloSessoes">Meus endereços</Typography>
           </Box>
-
 
           <FlatList
             showsVerticalScrollIndicator={false}
             data={addresses}
             keyExtractor={(item, index) => index.toString()}
             renderItem={({ item }) => {
-              const { address1, address2, address3, city, state, postalCode, id, firstName, phoneNumber, jobTitle } = item.address
-              const numberAndComplement: string[] | undefined = address2?.split("|");
+              const {
+                address1,
+                address2,
+                address3,
+                city,
+                state,
+                postalCode,
+                id,
+                firstName,
+                phoneNumber,
+                jobTitle,
+              } = item.address;
+              const numberAndComplement: string[] | undefined = address2?.split(
+                "|"
+              );
               return (
                 <AddressSelector
                   addressData={{
-                    address: `${address1}, ${numberAndComplement && numberAndComplement[0]}, ${numberAndComplement && numberAndComplement[1]}, ${address3}, ${city} - ${state}`,
+                    address: `${address1}, ${
+                      numberAndComplement && numberAndComplement[0]
+                    }, ${
+                      numberAndComplement && numberAndComplement[1]
+                    }, ${address3}, ${city} - ${state}`,
                     title: address1,
                     zipcode: postalCode,
                   }}
                   deleteAddress={() => {
                     setDeleteModal(true);
-                    setAddressId(id ? id : '')
+                    setAddressId(id ? id : "");
                   }}
                   edit={() => {
-                    navigation.navigate('NewAddress',
-                      {
-                        edit: true,
-                        editAddress: {
-                          id: id,
-                          postalCode: postalCode,
-                          state: state,
-                          city: city,
-                          street: address1,
-                          district: address3,
-                          numberAndComplement: address2?.split("|"),
-                          firstName: firstName,
-                          phoneNumber: phoneNumber,
-                          jobTitle: jobTitle
-                        }
-                      }
-                    );
+                    navigation.navigate("NewAddress", {
+                      edit: true,
+                      editAddress: {
+                        id: id,
+                        postalCode: postalCode,
+                        state: state,
+                        city: city,
+                        street: address1,
+                        district: address3,
+                        numberAndComplement: address2?.split("|"),
+                        firstName: firstName,
+                        phoneNumber: phoneNumber,
+                        jobTitle: jobTitle,
+                      },
+                    });
                   }}
                   selected={idAddress === id ? true : false}
                   select={() => {
                     if (isCheckout) {
-                      navigation.navigate('PaymentMethodScreen');
+                      navigation.navigate("PaymentMethodScreen");
                       return;
                     }
-                    setSelected(selected)
-                    setIdAddress(id)
-                    dispatch(createDefaultAddress(
-                      {
+                    setSelected(selected);
+                    setIdAddress(id);
+                    dispatch(
+                      createDefaultAddress({
                         address: {
                           country: "BR",
                           address3: address3,
@@ -196,29 +225,29 @@ const AddressList: React.FC<Props> = ({ route }) => {
                           firstName: firstName,
                           phoneNumber: phoneNumber,
                           jobTitle: jobTitle,
-                          id: id
-                        }
-                      }
-                    ))
+                          id: id,
+                        },
+                      })
+                    );
                     // navigation.navigate('NewAddress', {
                     //   isCheckout,
                     //   id: null,
                     // });
-                  }
-                  }
-                />)
+                  }}
+                />
+              );
             }}
           />
-          <Box marginX={'md'}>
+          <Box marginX={"md"}>
             <Button
               mt="xs"
               onPress={() =>
-                navigation.navigate('NewAddress', {
+                navigation.navigate("NewAddress", {
                   isCheckout,
                   id: null,
                 })
               }
-              title={'NOVO ENDEREÇO'}
+              title={"NOVO ENDEREÇO"}
               variant="primarioEstreitoOutline"
               padding="xl"
             />
@@ -227,7 +256,7 @@ const AddressList: React.FC<Props> = ({ route }) => {
         {isCheckout && (
           <Box justifyContent="flex-end" flex={1}>
             <Button
-              onPress={() => navigation.navigate('PaymentMethodScreen')}
+              onPress={() => navigation.navigate("PaymentMethodScreen")}
               title="FORMA DE PAGAMENTO"
               variant="primarioEstreito"
               inline={true}
