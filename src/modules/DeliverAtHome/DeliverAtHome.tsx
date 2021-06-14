29af1496e6188a648ff43543f4c8c6a680db95a0
gabriel.jesus <gabriel.jesus@globalsys.com>
2021-06-14 19:20:27 -0300

    review navigation checkout the address

---
+++ src/modules/DeliverAtHome/DeliverAtHome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography, Box } from "reserva-ui";
+
+const DeliverAtHome = () => {
+  return (
+    <Box bg={"amareloAtencao"}>
+      <Typography>Hello word</Typography>
+    </Box>
+  );
+};
+
+export default DeliverAtHome;
