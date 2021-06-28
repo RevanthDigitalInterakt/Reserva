d0b591dce5120125668f3b76d18e9f14f6a91e64
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2021-06-28 17:10:13 -0300

    searhcProduct

--- src/modules/Search/pages/Search.tsx
+++ src/modules/Search/pages/Search.tsx
@@ -1,34 +1,20 @@
+import { QueryResult, useQuery } from '@apollo/client'
 import { StackScreenProps } from '@react-navigation/stack'
 import * as React from 'react'
-import { useEffect } from 'react'
-import { Dimensions, ScrollView, TouchableOpacity } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { Dimensions } from 'react-native'
+import * as Animatable from 'react-native-animatable'
+import { useSelector } from 'react-redux'
 import {
-  Box,
-  theme,
-  Image,
-  ProductVerticalListCard,
-  Button,
-  Typography,
-  Icon,
-  Picker,
-  SearchBar,
-  Pill,
+  Box, SearchBar
 } from 'reserva-ui'
-import { images } from '../../../assets'
+import { ProductQL, productSearch, ProductSearchResponse, ProductSearchVars } from '../../../graphql/products/productSearch'
 import { RootStackParamList } from '../../../routes/StackNavigator'
 import { ApplicationState } from '../../../store'
-
+import { Product } from '../../../store/ducks/product/types'
 import { TopBarDefault } from '../../Menu/components/TopBarDefault'
-import { TopBarDefaultBackButton } from '../../Menu/components/TopBarDefaultBackButton'
-import { FilterModal } from '../../ProductCatalog/modals/FilterModal'
-import * as Animatable from 'react-native-animatable'
-import { loadProducts } from '../../../store/ducks/products/actions'
-import { loadProductsSuccess } from '../../../store/ducks/products/actions'
 import { ListVerticalProducts } from '../../ProductCatalog/components/ListVerticalProducts/ListVerticalProducts'
-import { loadAddressRequest } from '../../../store/ducks/address/sagas'
-import { set } from 'date-fns'
+
 
 const windowWidth = Dimensions.get('window').width
 
@@ -39,47 +25,84 @@ export const SearchScreen: React.FC<Props> = ({ route, navigation }) => {
   const [showResults, setShowResults] = React.useState(true)
 
   const { searchterm } = route.params
-  const dispatch = useDispatch()
 
   const [filterVisible, setFilterVisible] = React.useState(false)
   const [sorterVisible, setSorterVisible] = React.useState(false)
   const [filterList, setFilterList] = React.useState<string[]>([])
 
-  const products = useSelector((state: ApplicationState) => state.products)
+  const [products, setProducts] = useState<Product[]>()
   const filters = useSelector((state: ApplicationState) => state.filter.data)
+
+  let pageSize = 12
+  let { data: productData, loading, error, fetchMore, refetch }: QueryResult<ProductSearchResponse, ProductSearchVars> = useQuery(productSearch, {
+    variables: {
+      to: (pageSize - 1)
+    }
+  })
+
+  console.log('data', productData, 'error', error, 'loading', loading)
 
   useEffect(() => {
     setShowResults(false)
-    dispatch(loadProductsSuccess([]))
   }, [])
 
-  const loadMoreProducts = (offset: number, searchQuery: string) => {
-    console.log('loading more')
-    dispatch(
-      loadProducts({
-        categoryId: '',
-        limit: 10,
-        offset: offset,
-        searchQuery: searchQuery,
-        ...filters,
-      })
-    )
+  useEffect(() => {
+    productData?.productSearch.products && updateProductsArray(productData.productSearch.products)
+  }, [productData])
+
+  const handleSearch = async (text: string) => {
+    const { data } = await refetch({
+      fullText: text,
+
+    })
+
+    resetProductsArray()
+    updateProductsArray(data?.productSearch?.products || [])
+    setShowResults(true)
+  }
+
+  const updateProductsArray = (newArray: ProductQL[]) => {
+    let newProducts: Product[] | undefined = newArray.map(prod => {
+      return {
+        title: prod.productName,
+        currency: 'R$',
+        installmentPrice: prod.items && prod.items[0].sellers ? prod.items[0]?.sellers[0].commertialOffer?.Installments[0].Value : undefined,
+        installmentNumber: prod.items && prod.items[0].sellers ? prod.items[0]?.sellers[0].commertialOffer?.Installments[0].NumberOfInstallments : undefined,
+        fullPrice: prod.items && prod.items[0].sellers ? prod.items[0]?.sellers[0].commertialOffer?.Installments[0].TotalValuePlusInterestRate : undefined,
+        imageUrl: prod.items[0].images[0].imageUrl || ''
+      } as Product
+    })
+    setProducts(newProducts)
+  }
+
+  const resetProductsArray = () => {
+    setProducts([])
+  }
+
+  const loadMoreProducts = async (offset: number, searchQuery?: string) => {
+    console.log('loading more', offset, searchTerm)
+    let { data: {
+      productSearch: { products: newProducts }
+    } } = await fetchMore({
+      variables: {
+        form: offset < pageSize ? pageSize : offset,
+        to: offset < pageSize ? (pageSize * 2) - 1 : offset + (pageSize - 1),
+      }
+    })
+
+    updateProductsArray([...newProducts])
   }
 
   return (
     <Box backgroundColor='white' flex={1}>
-      <TopBarDefault loading={false} />
+      <TopBarDefault loading={loading} />
       <Box paddingX='nano' paddingBottom='micro' paddingTop='micro'>
         <SearchBar
           onValueChange={(text) => {
-            if (text == '' || text == undefined) {
-              setShowResults(false)
-            } else {
-              setShowResults(true)
-              setSearchTerm(text)
-              dispatch(loadProductsSuccess([]))
-              loadMoreProducts(0, text)
-            }
+            setSearchTerm(text)
+          }}
+          onClickIcon={() => {
+            handleSearch(searchTerm)
           }}
           height={36}
           placeholder='Buscar'
@@ -89,9 +112,7 @@ export const SearchScreen: React.FC<Props> = ({ route, navigation }) => {
       {showResults && (
         <Animatable.View animation='fadeIn' style={{ height: '100%' }}>
           <ListVerticalProducts
-            products={products.dataOffer.filter((x) =>
-              x.title.toUpperCase().includes(searchTerm.toUpperCase())
-            )}
+            products={products ? products : []}
             loadMoreProducts={(offset) => {
               loadMoreProducts(offset, searchTerm)
             }}
