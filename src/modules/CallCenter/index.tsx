a78f58d79f780e5012cdbc04d30b4a3e2aa1eccc
gabriel.jesus <gabriel.jesus@globalsys.com>
2021-08-06 17:01:26 -0300

    keyboard screen adress and call-center icon

---
+++ src/modules/CallCenter/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const CallCenter = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 18, fontWeight: "bold" }}>Loading...</Text>
+    </View>
+  );
+};
+
+export default CallCenter;
