01618c63c3943cf9c994cb772906308e458bea53
Danilo Machado de Sousa <danilomsou@gmail.com>
2021-09-06 16:54:57 -0300

    chore: integration appsflyer

--- src/InitialScreen.tsx
+++ src/InitialScreen.tsx
@@ -1,83 +1,18 @@
-import React, { useState } from "react";
-import { Children, Component, FC } from "react";
+import React from "react";
+import { FC } from "react";
 import { StatusBar, Platform } from 'react-native';
-import LottieView from "lottie-react-native";
-import { animations } from "./assets";
-import Animated from "react-native-reanimated";
 import * as Animatable from "react-native-animatable";
 import { SafeAreaView } from "react-native-safe-area-context";
-import AnimatedLottieView from "lottie-react-native";
 import { useEffect } from "react";
-import { restoreSession } from "./store/ducks/authentication/actions";
-import { useDispatch } from "react-redux";
-import { gql, useQuery } from '@apollo/client';
-import { productSearchQuery } from "./store/ducks/products/types";
 import SplashScreen from "react-native-splash-screen";
-
 const InitialScreen: React.FC<{ children: FC }> = ({ children }) => {
-  const dispatch = useDispatch();
-  const [loaded, setLoaded] = useState(false);
-  const [animation, setAnimation] = useState<AnimatedLottieView | null>(null);
 
   useEffect(() => {
     SplashScreen.hide()
   }, [])
 
-  useEffect(() => {
-    animation?.play();
-  }, [animation]);
-
-  useEffect(() => {
-    dispatch(restoreSession());
-  }, []);
-
-  useEffect(() => { }, [animation?.props.progress]);
-
-  const profileQuery = gql`
-  query {
-    appMenuCollection(limit: 1) {
-      items {
-        itemsCollection(limit: 100) {
-          items {
-            name
-            referenceId
-            childCategoryCollection(limit: 100) {
-              items {
-                name
-                referenceId
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-
-  const { loading, error, data } = useQuery(
-    profileQuery,
-    {
-      context: { clientName: 'contentful' }
-    }
-  );
-
-
   return (
     <>
-      {/* {!loaded ? (
-        <LottieView
-          style={{ flex: 1 }}
-          onAnimationFinish={() => {
-            setLoaded(true);
-          }}
-          ref={(animation) => {
-            setAnimation(animation);
-          }}
-          autoPlay
-          loop={false}
-          source={animations.splash}
-        />
-      ) : ( */}
       <SafeAreaView style={{ flex: 1 }}>
         <StatusBar
           animated={true}
@@ -87,7 +22,6 @@ const InitialScreen: React.FC<{ children: FC }> = ({ children }) => {
           {children}
         </Animatable.View>
       </SafeAreaView>
-      {/* // )} */}
     </>
   );
 };
