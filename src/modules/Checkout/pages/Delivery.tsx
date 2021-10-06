c1197ccb18101837b2d5e39c2885b6ac8cfdbb74
Tiago Rodrigues <tiiago.santosr@gmail.com>
2021-10-06 08:37:52 -0300

    feat: flow store

--- src/modules/Checkout/pages/Delivery.tsx
+++ src/modules/Checkout/pages/Delivery.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { SafeAreaView, ScrollView, Platform, FlatList, TouchableOpacity } from "react-native";
-import { Typography, Box, Button, Icon, Divider, Image } from "reserva-ui";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { Typography, Box, Button } from "reserva-ui";
 import { TopBarBackButton } from "../../Menu/components/TopBarBackButton";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { withAuthentication } from "../../Profile/HOC/withAuthentication";
@@ -28,7 +28,9 @@ import Store from "../components/Store";
 const Delivery: React.FC<{}> = () => {
   const navigation = useNavigation();
   const { orderForm, addShippingOrPickupInfo } = useCart();
-  const { cookie, setCookie } = useAuth()
+  const { cookie, setCookie } = useAuth();
+  const [Permission, setPermission] = useState(false);
+  const [mapPermission, setMapPermission] = useState(false);
   const { authentication } = useSelector((state: ApplicationState) => state);
   const [addresses, setAddresses] = useState<any[]>([]);
   const [selectedAddress, setSelectedAddress] = useState<any>(null);
@@ -38,8 +40,7 @@ const Delivery: React.FC<{}> = () => {
   const [typeOfDelivery, setTypeOfDelivery] = useState<any>([]);
   const [pickupPoint, setPickupPoint] = useState<any>();
   const [businessHours, setBusinessHours] = useState<any>([]);
-
-  const [selectMethodDelivery, setSelectMethodDelivery] = useState(false)
+  const [selectMethodDelivery, setSelectMethodDelivery] = useState(false);
   const [addressId, setAddressId] = React.useState("");
   const [loading, setLoading] = useState(false);
 
@@ -57,6 +58,48 @@ const Delivery: React.FC<{}> = () => {
       refetch();
     }
   });
+
+  //permissão para acessar o mapa
+  const requestMap = async () => {
+    try {
+      const lacationAlways = await request(
+        PERMISSIONS.IOS.LOCATION_ALWAYS);
+      const lacationInUse = await request(
+        PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+      );
+      const fineLoation = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+      )
+      if (lacationAlways === 'granted' || lacationInUse === 'granted' || fineLoation === 'granted') {
+        setPermission(true)
+      }
+    } catch (error) {
+    }
+  }
+
+  //permissão para acessar o mapa
+  const CkeckmapPermission = async () => {
+    try {
+      const check = await checkMultiple([
+        PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+        PERMISSIONS.IOS.LOCATION_ALWAYS,
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+      ]);
+      if (check["ios.permission.LOCATION_WHEN_IN_USE"] === 'granted' || check["ios.permission.LOCATION_ALWAYS"] === 'granted' || check["android.permission.ACCESS_FINE_LOCATION"] === "granted") {
+        setMapPermission(true)
+      }
+    } catch (err) {
+    }
+  }
+
+  useEffect(() => {
+    requestMap();
+  }, [])
+
+
+  useEffect(() => {
+    CkeckmapPermission();
+  }, [Permission])
 
   const onAddressChosen = (item: any) => {
     setSelectedAddress({ ...item, addressType: "residential" });
@@ -365,10 +408,10 @@ const Delivery: React.FC<{}> = () => {
             <Button
               justifyContent="flex-end"
               onPress={() =>
-                navigation.navigate("NewAddress", {
-                  isCheckout: true,
-                  id: null,
-                })
+                mapPermission ?
+                  navigation.navigate('MapScreen', { geolocation: "", locationPermission: mapPermission })
+                  :
+                  navigation.navigate("WithdrawInStore", { isCheckout: true, })
               }
               inline={true}
               title={"LOJAS PRÓXIMAS A SUA REGIÃO"}
@@ -392,184 +435,3 @@ const Delivery: React.FC<{}> = () => {
   );
 };
 export const DeliveryScreen = Delivery;
-
-interface IStore {
-  storeDetail: any[];
-  data: any[];
-}
-
-// const Store = ({ storeDetail, data }: IStore) => {
-//   const [mapPermission, setMapPermission] = useState(false)
-//   const navigation = useNavigation();
-//   const [showModalStore, setShowModalStore] = useState(false)
-//   const [pickupPoints, setPickupPoints] = useState([])
-//   const diasDaSemana = [
-//     'Segunda-feira',
-//     'Terça-feira',
-//     'Quarta-feira',
-//     'Quinta-feira',
-//     'Sexta-feira',
-//     'Sábado',
-//     'Domingo',
-//   ]
-//   // console.log('storeDetail', storeDetail)
-//   useEffect(() => {
-//     console.log('storeDetail', storeDetail)
-//   }, [storeDetail])
-
-//   return (
-//     <Box>
-//       <Box mt="xxs">
-//         <Typography
-//           fontFamily="reservaSansBold"
-//           fontSize={12}
-//           color="neutroFrio2"
-//         >
-//           LOJA MAIS PRÓXIMA
-//         </Typography>
-//         <Box
-//           flex={1}
-//           backgroundColor={'white'}
-//           my={'micro'}
-//           borderWidth="hairline"
-//           borderColor="divider"
-//           pt="micro"
-//           pb="xxxs"
-//           px="nano"
-//         >
-//           {pickupPoints ?
-//             <Box borderColor={'backgroundMenuOpened'}>
-//               <Box flexDirection="row">
-//                 <Box alignItems="center">
-//                   <Image
-//                     height={40}
-//                     source={images.localReserva}
-//                     resizeMode={'contain'}
-//                   />
-//                   <Typography
-//                     fontFamily="reservaSansMedium"
-//                     fontSize={12}
-//                   >
-//                     {+data.pickupDistance.toFixed(1)} km
-//                   </Typography>
-//                 </Box>
-//                 <Box flex={1}>
-//                   <Box mb={'quarck'}>
-//                     <Typography
-//                       fontFamily="reservaSansBold"
-//                       fontSize={14}
-//                     >
-//                       {data.pickupStoreInfo.friendlyName}
-
-//                     </Typography>
-//                   </Box>
-//                   <Box>
-//                     <Typography fontFamily="reservaSansRegular" fontSize={13}>
-//                       AV DOUTOR OLIVIO LIRA 353, LOJA 302 K/L PRAIA DA COSTA - VILA VELHA - ES.
-//                       {/* {`${item.address.street}, ${item.address.number}
-//                    ${item.address.complement} - ${item.address.neighborhood} - ${item.address.state}, ${item.address.postalCode}`} */}
-//                     </Typography>
-//                   </Box>
-//                   <Box flexDirection="row" mb="nano">
-//                     <Box mr="xxs">
-//                       <Typography
-//                         fontFamily="reservaSansMedium"
-//                         fontSize={12}
-//                         color="verdeSucesso"
-//                       >
-//                         Grátis
-//                       </Typography>
-//                     </Box>
-//                     <Typography
-//                       fontFamily="reservaSansMedium"
-//                       fontSize={12}
-//                       color="verdeSucesso"
-//                     >
-//                       Pronto em até {data.shippingEstimate?.split('bd')[0]} dias
-//                     </Typography>
-//                   </Box>
-//                   <Button
-//                     flex={1}
-//                     alignSelf="flex-start"
-//                     onPress={() => setShowModalStore(true)}
-//                   >
-//                     <Box
-//                       flex={1}
-//                       alignSelf="flex-start"
-//                     >
-//                       <Typography
-//                         style={{ textDecorationLine: "underline" }}
-//                         fontFamily="nunitoRegular"
-//                         fontSize={12}>
-//                         Detalhes da loja
-//                       </Typography>
-//                     </Box>
-//                   </Button>
-//                 </Box>
-//               </Box>
-//             </Box> : null}
-//         </Box>
-//         <Modal
-//           isVisible={showModalStore}
-//         >
-//           <Box
-//             bg='white'
-//             p="xxxs"
-//           >
-//             <Box
-//               flexDirection="row"
-//               alignItems="center"
-//               justifyContent="space-between"
-//             >
-//               <Box>
-//                 <Typography
-//                   fontFamily="reservaSerifRegular"
-//                   fontSize={20}
-//                 >
-//                   Detalhes da Loja
-//                 </Typography>
-//               </Box>
-//               <Button
-//                 hitSlop={{
-//                   top: 30,
-//                   bottom: 30,
-//                   right: 30,
-//                   left: 30,
-//                 }}
-//                 onPress={() => setShowModalStore(false)}
-//                 variant='icone'
-//                 icon={
-//                   <Icon size={12} name='Close' />
-//                 }
-//               />
-//             </Box>
-//             <Box mt="xxs" mb="micro">
-//               <Typography fontFamily="reservaSansMedium" fontSize={14}>
-//                 Horários de funcionamento
-//               </Typography>
-//             </Box>
-//             {storeDetail &&
-//               storeDetail.map((item) => (
-//                 <>
-//                   <Box
-//                     py="nano"
-//                     flexDirection="row"
-//                     justifyContent="space-between"
-//                   >
-//                     <Typography fontFamily="reservaSansLight" fontSize={14}>
-//                       {diasDaSemana[item.DayOfWeek]}
-//                     </Typography>
-//                     <Typography fontFamily="reservaSansRegular" fontSize={14}>
-//                       {item.OpeningTime} às {item.ClosingTime}
-//                     </Typography>
-//                   </Box>
-//                   <Divider variant="fullWidth" />
-//                 </>
-//               ))
-//             }
-//           </Box>
-//         </Modal>
-//       </Box>
-//     </Box>
-//   );
-// }
