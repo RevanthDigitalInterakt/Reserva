4773a8e2e3fc0ce5c543a4aed7171af08e4f21af
VlademirMelo <vlademir.melo@globalsys.com.br>
2021-10-06 17:12:31 -0300

    feat: add signature conditions

--- src/modules/ProductDetail/pages/ProductDetail.tsx
+++ src/modules/ProductDetail/pages/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import React, { createRef, useEffect, useState } from 'react';
-import { Alert, Dimensions, KeyboardAvoidingView, Platform } from 'react-native';
+import { Alert, Dimensions, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {
@@ -185,6 +185,7 @@ export const ProductDetail: React.FC<Props> = ({
   const [isVisible, setIsVisible] = useState(false);
   const [skip, setSkip] = useState(false)
   const [loadingFavorite, setLoadingFavorite] = useState(false)
+  const [acceptConditions, setAcceptConditions] = useState(false)
   const [wishInfo, setWishInfo] = useState({
     listIds: [''],
     inList: false
@@ -683,6 +684,256 @@ export const ProductDetail: React.FC<Props> = ({
                     inline
                   />
                   <Box mt="nano" flexDirection="row"></Box>
+                  <Divider variant="fullWidth" my="xs" />
+
+                  {/* CHECKLIST INFO */}
+
+                  <Box
+                    flexDirection='row'
+                    alignItems='center'
+                    mb='xxxs'
+                  >
+                    <Box
+                      alignItems='center'
+                      justifyContent='center'
+                      backgroundColor='verdeSucesso'
+                      width={20}
+                      height={20}
+                      borderRadius='xxxs'
+                      mr='micro'
+                    >
+                      <Icon name="Check" size={18} color='white' mt='nano' ml='quarck' />
+                    </Box>
+
+                    <Box>
+                      <Box
+                        flexDirection='row'
+                      >
+                        <Typography variant='tituloSessao'>Receba </Typography>
+
+                        <Typography variant='tituloSessao' fontWeight='bold'>3 camisetas </Typography>
+
+                        <Typography variant='tituloSessao'>nos 12 meses de</Typography>
+                      </Box>
+
+                      <Typography variant='tituloSessao'>assinatura.</Typography>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    flexDirection='row'
+                    mb='xxxs'
+                    alignItems='center'
+                  >
+                    <Box
+                      alignItems='center'
+                      justifyContent='center'
+                      backgroundColor='verdeSucesso'
+                      width={20}
+                      height={20}
+                      borderRadius='xxxs'
+                      mr='micro'
+                    >
+                      <Icon name="Check" size={18} color='white' mt='nano' ml='quarck' />
+                    </Box>
+
+                    <Box
+                      flexDirection='row'
+                      alignItems='center'
+                    >
+                      <Typography variant='tituloSessao' fontWeight='bold'>Ganhe 100% </Typography>
+
+                      <Typography variant='tituloSessao'>de </Typography>
+
+                      <Typography variant='tituloSessao' fontStyle='italic' >cashback </Typography>
+
+                      <Box
+                        flexDirection='row'
+                        alignSelf='flex-start'
+                        mb='nano'
+                      >
+                        <Typography fontSize={3} >*</Typography>
+                        <Typography fontSize={2} >1</Typography>
+                      </Box>
+
+                      <Typography fontSize={2} >.</Typography>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    flexDirection='row'
+                    alignItems='center'
+                    mb='xxxs'
+                  >
+                    <Box
+                      alignItems='center'
+                      justifyContent='center'
+                      backgroundColor='verdeSucesso'
+                      width={20}
+                      height={20}
+                      borderRadius='xxxs'
+                      mr='micro'
+                    >
+                      <Icon name="Check" size={18} color='white' mt='nano' ml='quarck' />
+                    </Box>
+
+                    <Box>
+                      <Box
+                        flexDirection='row'
+                        alignItems='center'
+                      >
+                        <Typography variant='tituloSessao' fontWeight='bold'>Receba 20% OFF </Typography>
+
+                        <Typography variant='tituloSessao'>em todas as compras</Typography>
+
+                        <Box
+                          flexDirection='row'
+                          alignSelf='flex-start'
+                          mb='nano'
+                        >
+                          <Typography fontSize={3} >*</Typography>
+                          <Typography fontSize={2} >2</Typography>
+                        </Box>
+                      </Box>
+
+                      <Typography variant='tituloSessao'>acima de R$ 399.</Typography>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    flexDirection='row'
+                    alignItems='center'
+                    mb='xxxs'
+                  >
+                    <Box
+                      alignItems='center'
+                      justifyContent='center'
+                      backgroundColor='verdeSucesso'
+                      width={20}
+                      height={20}
+                      borderRadius='xxxs'
+                      mr='micro'
+                    >
+                      <Icon name="Check" size={18} color='white' mt='nano' ml='quarck' />
+                    </Box>
+
+                    <Box>
+                      <Box
+                        flexDirection='row'
+                      >
+                        <Typography variant='tituloSessao' fontWeight='bold'>Ganhe R$ 75 </Typography>
+
+                        <Typography variant='tituloSessao'>em créditos ao fim da anuidade, </Typography>
+                      </Box>
+
+                      <Box
+                        flexDirection='row'
+                      >
+                        <Typography variant='tituloSessao'>caso queira devolver as 3 camisetas</Typography>
+
+                        <Box
+                          flexDirection='row'
+                          alignSelf='flex-start'
+                          mb='nano'
+                        >
+                          <Typography fontSize={3} >*</Typography>
+                          <Typography fontSize={2} >3</Typography>
+                        </Box>
+
+                        <Typography variant='tituloSessao'>.</Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    flexDirection='row'
+                    alignItems='center'
+                    mb='xxs'
+                  >
+                    <Box
+                      alignItems='center'
+                      justifyContent='center'
+                      backgroundColor='verdeSucesso'
+                      width={20}
+                      height={20}
+                      borderRadius='xxxs'
+                      mr='micro'
+                    >
+                      <Icon name="Check" size={18} color='white' mt='nano' ml='quarck' />
+                    </Box>
+
+                    <Box>
+                      <Typography variant='tituloSessao'>Ciclo sustentável: as peças devolvidas serão </Typography>
+
+                      <Typography variant='tituloSessao'>recicladas.</Typography>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    p='nano'
+                    backgroundColor='backgoundDivider'
+                  >
+                    <Box
+                      flexDirection='row'
+                    >
+                      <Typography variant='precoAntigo3' fontSize={1} color='searchBarTextColor'>*1</Typography>
+                      <Typography variant='precoAntigo3' color='searchBarTextColor'>: Créditos mensais não cumulativos, expiram a cada 30</Typography>
+                    </Box>
+
+                    <Typography variant='precoAntigo3' color='searchBarTextColor'>dias.</Typography>
+
+                    <Box
+                      flexDirection='row'
+                      mt='quarck'
+                    >
+                      <Typography variant='precoAntigo3' fontSize={1} color='searchBarTextColor'>*2</Typography>
+                      <Typography variant='precoAntigo3' color='searchBarTextColor'>: 20% de desconto exceto para itens já em promoção.</Typography>
+                    </Box>
+
+                    <Box
+                      flexDirection='row'
+                      mt='quarck'
+                    >
+                      <Typography variant='precoAntigo3' fontSize={1} color='searchBarTextColor'>*3</Typography>
+                      <Typography variant='precoAntigo3' color='searchBarTextColor'>: Ao final da anuidade, crédito de R$ 25 por camiseta</Typography>
+                    </Box>
+
+                    <Typography variant='precoAntigo3' color='searchBarTextColor'>SimplesⓇ devolvida em lojas Reserva</Typography>
+                  </Box>
+
+                  <Box
+                    flexDirection='row'
+                    alignItems='center'
+                    mt='xxxs'
+                  >
+                    <TouchableOpacity
+                      onPress={() => setAcceptConditions(!acceptConditions)}
+                    >
+                      <Box
+                        backgroundColor={acceptConditions ? 'preto' : 'white'}
+                        width={14}
+                        height={14}
+                        border='1px'
+                        borderColor='preto'
+                        borderRadius='pico'
+                        mr='nano'
+                        alignItems='center'
+                        justifyContent='center'
+                      >
+                        {acceptConditions && <Icon name="Check" size={14} color='white' mt='nano' ml='quarck' />}
+                      </Box>
+                    </TouchableOpacity>
+
+                    <Typography variant='precoAntigo3' color='preto'>Ao adquirir a assinatura você aceita os </Typography>
+
+                    <Typography
+                      variant='precoAntigo3'
+                      color='preto'
+                      fontWeight='bold'
+                      style={{ textDecorationLine: 'underline' }}
+                    >termos e condições.</Typography>
+                  </Box>
+
                   <Divider variant="fullWidth" my="xs" />
 
                   {/* DELIVERY INFO */}
