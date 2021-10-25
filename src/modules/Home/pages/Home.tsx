98ee2e272fea194cc0b4cb11e5c53fcc95a17c29
Cleiber Reis <cleiber.reis@globalsys.com.br>
2021-10-24 23:13:14 -0300

    fix: wish-list-storage

--- src/modules/Home/pages/Home.tsx
+++ src/modules/Home/pages/Home.tsx
@@ -1,81 +1,87 @@
+import React, { useState, useLayoutEffect, useEffect } from 'react';
+
 import { useLazyQuery, useQuery } from '@apollo/client';
+import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
-import React, { useState } from 'react';
-import { useLayoutEffect } from 'react';
-import { useEffect } from 'react';
 import { Dimensions, Platform } from 'react-native';
+import { View } from 'react-native-animatable';
 import { FlatList, TouchableHighlight } from 'react-native-gesture-handler';
 import { useDispatch } from 'react-redux';
 import { Box, Image, Typography, Button, Icon } from 'reserva-ui';
+
 import { useAuth } from '../../../context/AuthContext';
+import { useCheckConnection } from '../../../shared/hooks/useCheckConnection';
 import { homeQuery, HomeQuery } from '../../../store/ducks/HomePage/types';
 import { load } from '../../../store/ducks/nearbyStores/actions';
 import { profileQuery } from '../../../store/ducks/profile/types';
 import { TopBarDefault } from '../../Menu/components/TopBarDefault';
-import { View } from 'react-native-animatable';
-import { useCheckConnection } from '../../../shared/hooks/useCheckConnection';
-import AsyncStorage from '@react-native-community/async-storage';
-import { DiscoutCodeModal } from '../component/DiscoutCodeModal';
 import { StoreUpdate } from '../../Update/pages/StoreUpdate';
+import { DiscoutCodeModal } from '../component/DiscoutCodeModal';
 
-
-const { width: screenWidth, height: screenHeight } = Dimensions.get('window')
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
 export const HomeScreen: React.FC<{
   title: string;
 }> = ({ children, title }) => {
   const navigation = useNavigation();
-  const { cookie, setEmail } = useAuth()
-  const [modalCodeIsVisible, setModalCodeIsVisible] = useState(true)
-  const [isVisibleUpdateStore, setIsVisibleUpdateStore] = useState(false)
-  const [getProfile, { data: profileData, loading: profileLoading }] = useLazyQuery(profileQuery);
+  const { cookie, setEmail } = useAuth();
+  const [modalCodeIsVisible, setModalCodeIsVisible] = useState(true);
+  const [isVisibleUpdateStore, setIsVisibleUpdateStore] = useState(false);
+  const [getProfile, { data: profileData, loading: profileLoading }] =
+    useLazyQuery(profileQuery);
   const dispatch = useDispatch();
-  const [images, setImages] = React.useState<HomeQuery[]>([])
-  const [hasInternet, setHasInternet] = React.useState<boolean>(false)
+  const [images, setImages] = React.useState<HomeQuery[]>([]);
+  const [hasInternet, setHasInternet] = React.useState<boolean>(false);
   const deviceWidth = Dimensions.get('screen').width;
-  const { loading, error, data, refetch } = useQuery(
-    homeQuery,
-    {
-      context: { clientName: 'contentful' },
-      variables: { limit: 0 } // quantidade de itens que iram renderizar
-    }
-  );
-  const { WithoutInternet } = useCheckConnection({ refetch: refetch })
+  const { loading, error, data, refetch } = useQuery(homeQuery, {
+    context: { clientName: 'contentful' },
+    variables: { limit: 0 }, // quantidade de itens que iram renderizar
+  });
+  const { WithoutInternet } = useCheckConnection({ refetch });
 
   useEffect(() => {
-    let arrayImages = data?.homePageCollection.items[0].mediasCollection.items.map((imageDescription: any) => {
-      return {
-        fileName: imageDescription.image.fileName,
-        title: imageDescription.image.title,
-        width: imageDescription.image.width,
-        height: imageDescription.image.height,
-        size: imageDescription.image.size,
-        url: imageDescription.image.url,
-        reference: imageDescription.reference,
-      }
-    })
-    setImages(arrayImages)
+    const arrayImages =
+      data?.homePageCollection.items[0].mediasCollection.items.map(
+        (imageDescription: any) => ({
+          fileName: imageDescription.image.fileName,
+          title: imageDescription.image.title,
+          width: imageDescription.image.width,
+          height: imageDescription.image.height,
+          size: imageDescription.image.size,
+          url: imageDescription.image.url,
+          reference: imageDescription.reference,
+        })
+      );
+    setImages(arrayImages);
   }, [data]);
 
   useLayoutEffect(() => {
     if (cookie !== null) {
-      getProfile()
+      getProfile();
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (profileData) {
-      AsyncStorage.setItem('@RNAuth:email', profileData?.profile?.email)
+      AsyncStorage.setItem('@RNAuth:email', profileData?.profile?.email);
     }
-  }, [profileData])
-
+  }, [profileData]);
 
   useEffect(() => {
     dispatch(load({ UF: 'RJ' }));
   }, []);
 
+  useEffect(() => {
+    async function getStorage() {
+      const wishListData = await AsyncStorage.getItem('@WishList');
+
+      console.log('WISH HOME>>>>', JSON.parse(wishListData));
+    }
+    getStorage();
+  }, []);
+
   return (
-    <Box flex={1} bg='white'>
+    <Box flex={1} bg="white">
       <TopBarDefault loading={loading} />
       {/* <Box
         minHeight={40}
@@ -105,57 +111,65 @@ export const HomeScreen: React.FC<{
         </Button>
       </Box> */}
       <StoreUpdate />
-      <DiscoutCodeModal isVisible={modalCodeIsVisible} code={'RSVAPP50'} onClose={() => { setModalCodeIsVisible(false) }} />
+      <DiscoutCodeModal
+        isVisible={modalCodeIsVisible}
+        code="RSVAPP50"
+        onClose={() => {
+          setModalCodeIsVisible(false);
+        }}
+      />
       <WithoutInternet />
-      {
-        loading ?
-          <></>
-          // <Box flex={1}>
-          //   <SkeletonPlaceholder>
-          //     <SkeletonPlaceholder.Item width={screenWidth} height={screenHeight}>
-          //     </SkeletonPlaceholder.Item>
-          //   </SkeletonPlaceholder>
-          // </Box>
-          :
-          <FlatList
-            data={images}
-            renderItem={({ item }) => (
-              <Box alignItems="flex-start">
-                <Box mb="quarck" width={1 / 1} >
-                  <TouchableHighlight
-                    onPress={() => {
-                      let facetInput = []
-                      const [categoryType, categoryData] = item.reference.split(':')
-                      if (categoryType === 'category') {
-
-                        categoryData.split('|').forEach((cat: string) => {
-                          facetInput.push({
-                            key: 'c',
-                            value: cat
-                          })
-                        })
-                      } else {
+      {loading ? (
+        <></>
+      ) : (
+        // <Box flex={1}>
+        //   <SkeletonPlaceholder>
+        //     <SkeletonPlaceholder.Item width={screenWidth} height={screenHeight}>
+        //     </SkeletonPlaceholder.Item>
+        //   </SkeletonPlaceholder>
+        // </Box>
+        <FlatList
+          data={images}
+          renderItem={({ item }) => (
+            <Box alignItems="flex-start">
+              <Box mb="quarck" width={1 / 1}>
+                <TouchableHighlight
+                  onPress={() => {
+                    const facetInput = [];
+                    const [categoryType, categoryData] =
+                      item.reference.split(':');
+                    if (categoryType === 'category') {
+                      categoryData.split('|').forEach((cat: string) => {
                         facetInput.push({
-                          key: 'productClusterIds',
-                          value: categoryData
-                        })
-                      }
-                      navigation.navigate('ProductCatalog', { facetInput, referenceId: item.reference });
-                    }}
-                  >
-                    <Image
-                      height={item.height}
-                      autoHeight={true}
-                      width={deviceWidth}
-                      source={{ uri: item.url }}
-                    />
-                  </TouchableHighlight>
-                </Box>
+                          key: 'c',
+                          value: cat,
+                        });
+                      });
+                    } else {
+                      facetInput.push({
+                        key: 'productClusterIds',
+                        value: categoryData,
+                      });
+                    }
+                    navigation.navigate('ProductCatalog', {
+                      facetInput,
+                      referenceId: item.reference,
+                    });
+                  }}
+                >
+                  <Image
+                    height={item.height}
+                    autoHeight
+                    width={deviceWidth}
+                    source={{ uri: item.url }}
+                  />
+                </TouchableHighlight>
               </Box>
-            )}
-            keyExtractor={item => item.id}
-          />
-      }
+            </Box>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      )}
     </Box>
   );
 };
