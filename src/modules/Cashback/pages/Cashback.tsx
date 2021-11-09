80c19d1a3880cf8a1dced8d48118e05a788e25f0
Danilo Machado de Sousa <danilo.sousa@globalsys.com.br>
2021-11-09 16:50:23 -0300

    feat: add remote config in cashback

--- src/modules/Cashback/pages/Cashback.tsx
+++ src/modules/Cashback/pages/Cashback.tsx
@@ -1,72 +1,66 @@
-import { StackScreenProps } from '@react-navigation/stack'
-import React, { useState, useRef } from 'react'
-import { useEffect } from 'react'
+import React, { useState } from 'react';
+
+import { StackScreenProps } from '@react-navigation/stack';
 import { TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Box, Button, Divider, Typography, Alert, Icon } from 'reserva-ui'
-import { RootStackParamList } from '../../../routes/StackNavigator'
-import { PriceCustom } from '../../Checkout/components/PriceCustom'
-import { TopBarBackButton } from '../../Menu/components/TopBarBackButton'
+import Modal from 'react-native-modal';
 import QRCode from 'react-native-qrcode-svg';
-import { images } from '../../../assets'
-import { GenerateToken } from '../../../services/unicoService'
-import Modal from "react-native-modal";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Box, Button, Typography, Icon } from 'reserva-ui';
 
-type Props = StackScreenProps<RootStackParamList, 'Cashback'>
+import { images } from '../../../assets';
+import { RootStackParamList } from '../../../routes/StackNavigator';
+import { GenerateToken } from '../../../services/unicoService';
+import { TopBarBackButton } from '../../Menu/components/TopBarBackButton';
 
-export const Cashback: React.FC<Props> = ({ navigation, route }) => {
-  const [loading, setLoading] = useState(false)
-  const { credits } = route.params
+type Props = StackScreenProps<RootStackParamList, 'Cashback'>;
+
+export const Cashback: React.FC<Props> = ({ navigation }) => {
+  const [loading] = useState(false);
   const [token, setToken] = useState();
-  const [acceptConditions, setAcceptConditions] = useState(false)
-  const modalRef = useRef(false);
-  const [showModalTermsAndConditions, setShowModalTermsAndConditions] = useState(false);
-  const [successModal, setSuccessModal] = useState(false);
+  const [acceptConditions, setAcceptConditions] = useState(false);
+  const [showModalTermsAndConditions, setShowModalTermsAndConditions] =
+    useState(false);
 
-  const [loadingModal, setLoadingModal] = useState(false);
   const generateToken = async () => {
     const { data } = await GenerateToken();
-    setToken(data.access_token)
-  }
+    setToken(data.access_token);
+  };
 
   return (
-    <SafeAreaView flex={1} backgroundColor='white'>
+    <SafeAreaView flex={1} backgroundColor="white">
       <TopBarBackButton
         loading={loading}
         showShadow
         backButtonPress={() => navigation.goBack()}
       />
       <ScrollView>
-        <Box mx={20} mt='sm'>
-          <Box mb='nano'>
-            <Typography variant='tituloSessoes'>Ganhe Cashback na Hora</Typography>
+        <Box mx={20} mt="sm">
+          <Box mb="nano">
+            <Typography variant="tituloSessoes">
+              Ganhe Cashback na Hora
+            </Typography>
           </Box>
           <Box mb="xxs">
-            <Typography fontFamily='nunitoRegular' fontSize={14}>
-              Ao finalizar sua compra nas lojas físicas, você pode converter parte do valor em cashback!
+            <Typography fontFamily="nunitoRegular" fontSize={14}>
+              Ao finalizar sua compra nas lojas físicas, você pode converter
+              parte do valor em cashback!
             </Typography>
           </Box>
 
           <Box>
-            <Typography fontFamily='nunitoRegular' fontSize={14}>
-              Confira as regras da promoção atual com o vendedor e gere seu QR Code abaixo.
+            <Typography fontFamily="nunitoRegular" fontSize={14}>
+              Confira as regras da promoção atual com o vendedor e gere seu QR
+              Code abaixo.
             </Typography>
           </Box>
-          <Box
-            mt="xl"
-            alignItems="center"
-            justifyContent="center"
-          >
+          <Box mt="xl" alignItems="center" justifyContent="center">
             <ImageBackground
               source={images.QrcodeBackground}
-              style={{ width: 230, height: 230, justifyContent: "center" }}
+              style={{ width: 230, height: 230, justifyContent: 'center' }}
               resizeMode="contain"
             >
-              {token &&
-                <Box
-                  alignItems="center"
-                  justifyContent="center"
-                >
+              {token && (
+                <Box alignItems="center" justifyContent="center">
                   <QRCode
                     value={token}
                     logo={images.logoQRCode}
@@ -74,8 +68,7 @@ export const Cashback: React.FC<Props> = ({ navigation, route }) => {
                     size={200}
                   />
                 </Box>
-              }
-
+              )}
             </ImageBackground>
           </Box>
 
@@ -85,9 +78,9 @@ export const Cashback: React.FC<Props> = ({ navigation, route }) => {
               setIsVisible={() => setShowModalTermsAndConditions(false)}
             />
             <Box
-              flexDirection='row'
-              alignItems='center'
-              mt='xxxs'
+              flexDirection="row"
+              alignItems="center"
+              mt="xxxs"
               justifyContent="center"
             >
               <TouchableOpacity
@@ -97,123 +90,132 @@ export const Cashback: React.FC<Props> = ({ navigation, route }) => {
                   backgroundColor={acceptConditions ? 'preto' : 'white'}
                   width={14}
                   height={14}
-                  border='1px'
-                  borderColor='preto'
-                  borderRadius='pico'
-                  mr='nano'
-                  alignItems='center'
-                  justifyContent='center'
+                  border="1px"
+                  borderColor="preto"
+                  borderRadius="pico"
+                  mr="nano"
+                  alignItems="center"
+                  justifyContent="center"
                 >
-                  {acceptConditions && <Icon name="Check" size={14} color='white' mt='nano' ml='quarck' />}
+                  {acceptConditions && (
+                    <Icon
+                      name="Check"
+                      size={14}
+                      color="white"
+                      mt="nano"
+                      ml="quarck"
+                    />
+                  )}
                 </Box>
               </TouchableOpacity>
 
               <Box>
-                <Box
-                  flexDirection='row'
-                  alignItems='center'
-                >
-                  <Typography variant='precoAntigo3' color='preto'>Li e aceito os </Typography>
+                <Box flexDirection="row" alignItems="center">
+                  <Typography variant="precoAntigo3" color="preto">
+                    Li e aceito os{' '}
+                  </Typography>
 
                   <TouchableOpacity
                     onPress={() => setShowModalTermsAndConditions(true)}
                   >
                     <Typography
-                      variant='precoAntigo3'
-                      color='preto'
-                      fontWeight='bold'
+                      variant="precoAntigo3"
+                      color="preto"
+                      fontWeight="bold"
                       style={{ textDecorationLine: 'underline' }}
-                    >termos e condições.</Typography>
+                    >
+                      termos e condições.
+                    </Typography>
                   </TouchableOpacity>
                 </Box>
               </Box>
             </Box>
 
-            {token ?
-              <Box alignItems="center" justifyContent="center" mt="xs" mb="nano">
+            {token ? (
+              <Box
+                alignItems="center"
+                justifyContent="center"
+                mt="xs"
+                mb="nano"
+              >
                 <Typography
                   fontFamily="nunitoSemiBold"
                   fontSize={14}
                   color="verdeSucesso"
-                >QR Code gerado com sucesso!
+                >
+                  QR Code gerado com sucesso!
                 </Typography>
               </Box>
-              :
+            ) : (
               <Box mt="xs" mb="nano">
                 <Button
                   disabled={!acceptConditions}
                   onPress={() => generateToken()}
-                  title={"GERAR QR CODE"}
+                  title="GERAR QR CODE"
                   variant="primarioMaior"
                 />
               </Box>
-            }
+            )}
           </Box>
         </Box>
       </ScrollView>
       <ModalSuccess />
-
     </SafeAreaView>
-  )
-}
+  );
+};
 
 interface IModal {
   isVisible: boolean;
   setIsVisible: () => void;
 }
 
-const ModalTermsAndConditions = ({ isVisible, setIsVisible }: IModal) => {
-  return (
-    <Modal
-      avoidKeyboard
-      onBackdropPress={setIsVisible}
-      isVisible={isVisible}
-    >
-      <Box
-        bg="white"
-        marginY="xxl"
-        justifyContent="center"
-        px="xxxs"
-        py="xxxs"
-      >
-        <Box position='absolute' top={16} right={20} zIndex={4}>
-          <Button
-            onPress={setIsVisible}
-            variant='icone'
-            icon={
-              <Icon size={12} name='Close' />
-            }
-          />
-        </Box>
+const ModalTermsAndConditions = ({ isVisible, setIsVisible }: IModal) => (
+  <Modal avoidKeyboard onBackdropPress={setIsVisible} isVisible={isVisible}>
+    <Box bg="white" marginY="xxl" justifyContent="center" px="xxxs" py="xxxs">
+      <Box position="absolute" top={16} right={20} zIndex={4}>
+        <Button
+          onPress={setIsVisible}
+          variant="icone"
+          icon={<Icon size={12} name="Close" />}
+        />
+      </Box>
+      <Box mt="xxxs">
+        <Typography fontFamily="reservaSerifRegular" fontSize={20}>
+          Termos e condições
+        </Typography>
+      </Box>
+      <ScrollView>
         <Box mt="xxxs">
-          <Typography
-            fontFamily="reservaSerifRegular"
-            fontSize={20}
-          >
-            Termos e condições
+          <Typography fontFamily="nunitoRegular" fontSize={15}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
+            in laying out print, graphic or web designs. The passage is
+            attributed to an unknown typesetter in the 15th century who is
+            thought to have scrambled parts of Cicero’s De Finibus Bonorum et
+            Malorum for use in a type specimen book. It usually begins with. The
+            passage experienced a surge in popularity during the 1960s when
+            Letraset used it on their dry-transfer sheets, and again during the
+            90s as desktop publishers bundled the text with their software.
+            Today it’s seen all around the web; on templates, websites, and
+            stock designs. Use our generator to get your own, or read on for the
+            authoritative history of lorem ipsum.
           </Typography>
         </Box>
-        <ScrollView>
-          <Box mt="xxxs">
-            <Typography
-              fontFamily="nunitoRegular"
-              fontSize={15}
-            >
-              Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero’s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with.
-
-              The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it’s seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
-            </Typography>
-          </Box>
-        </ScrollView>
-        <Box width="100%" mt="micro">
-          <Button bg="verdeSucesso" width="100%" height={50} onPress={setIsVisible}>
-            <Typography color="white" fontFamily="nunitoSemiBold" fontSize={13}>OPA, VALEU!</Typography>
-          </Button>
-        </Box>
+      </ScrollView>
+      <Box width="100%" mt="micro">
+        <Button
+          bg="verdeSucesso"
+          width="100%"
+          height={50}
+          onPress={setIsVisible}
+        >
+          <Typography color="white" fontFamily="nunitoSemiBold" fontSize={13}>
+            OPA, VALEU!
+          </Typography>
+        </Button>
       </Box>
-    </Modal>
-  );
-}
+    </Box>
+  </Modal>
+);
 
 const ModalSuccess = () => {
   const [showModal, setShowModal] = useState(false);
@@ -231,37 +233,36 @@ const ModalSuccess = () => {
         px="xxxs"
         py="xxxs"
       >
-        <Box position='absolute' top={16} right={20} zIndex={4}>
+        <Box position="absolute" top={16} right={20} zIndex={4}>
           <Button
             onPress={() => setShowModal(false)}
-            variant='icone'
-            icon={
-              <Icon size={12} name='Close' />
-            }
+            variant="icone"
+            icon={<Icon size={12} name="Close" />}
           />
         </Box>
         <Box mt="xxxs">
-          <Typography
-            fontFamily="reservaSerifBold"
-            fontSize={24}
-          >
+          <Typography fontFamily="reservaSerifBold" fontSize={24}>
             Parabéns!
           </Typography>
         </Box>
         <Box mt="xxxs">
-          <Typography
-            fontFamily="reservaSerifRegular"
-            fontSize={24}
-          >
+          <Typography fontFamily="reservaSerifRegular" fontSize={24}>
             Você ganhou cashback.
           </Typography>
         </Box>
         <Box width="100%" mt="micro">
-          <Button bg="verdeSucesso" width="100%" height={50} onPress={() => setShowModal(false)}>
-            <Typography color="white" fontFamily="nunitoSemiBold" fontSize={13}>OPA, VALEU!</Typography>
+          <Button
+            bg="verdeSucesso"
+            width="100%"
+            height={50}
+            onPress={() => setShowModal(false)}
+          >
+            <Typography color="white" fontFamily="nunitoSemiBold" fontSize={13}>
+              OPA, VALEU!
+            </Typography>
           </Button>
         </Box>
       </Box>
     </Modal>
   );
-}
+};
--- src/modules/Credits/pages/Credits.tsx
+++ src/modules/Credits/pages/Credits.tsx
@@ -1,97 +1,112 @@
-import { StackScreenProps } from '@react-navigation/stack'
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Box, Divider, Typography, Icon, Button } from 'reserva-ui'
-import { RootStackParamList } from '../../../routes/StackNavigator'
-import { FetchCredit } from '../../../services/unicoService'
-import { useQuery } from "@apollo/client";
-import { profileQuery, ProfileVars } from "../../../graphql/profile/profileQuery";
-import { PriceCustom } from '../../Checkout/components/PriceCustom'
-import { TopBarBackButton } from '../../Menu/components/TopBarBackButton'
-import { useAuth } from '../../../context/AuthContext'
+import React, { useState, useEffect } from 'react';
 
-type Props = StackScreenProps<RootStackParamList, 'Credits'>
+import { useQuery } from '@apollo/client';
+import remoteConfig from '@react-native-firebase/remote-config';
+import { StackScreenProps } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Box, Divider, Typography, Icon, Button } from 'reserva-ui';
+
+import { useAuth } from '../../../context/AuthContext';
+import {
+  profileQuery,
+  ProfileVars,
+} from '../../../graphql/profile/profileQuery';
+import { RootStackParamList } from '../../../routes/StackNavigator';
+import { FetchCredit } from '../../../services/unicoService';
+import { PriceCustom } from '../../Checkout/components/PriceCustom';
+import { TopBarBackButton } from '../../Menu/components/TopBarBackButton';
+
+type Props = StackScreenProps<RootStackParamList, 'Credits'>;
 
 export const Credits: React.FC<Props> = ({ navigation, route }) => {
-    const { loading, error, data, refetch } = useQuery(profileQuery);
-    const [loadingCredit, setLoadingCredit] = useState(false)
-    const [profile, setProfile] = useState<ProfileVars>();
-    const [credit, SetCredit] = useState(0);
-    const { cleanEmailAndCookie } = useAuth()
+  const { loading, error, data, refetch } = useQuery(profileQuery);
+  const [loadingCredit, setLoadingCredit] = useState(false);
+  const [cashbackInStore, setCashbackInStore] = useState(false);
+  const [profile, setProfile] = useState<ProfileVars>();
+  const [credit, SetCredit] = useState(0);
+  const { cleanEmailAndCookie } = useAuth();
 
-    useEffect(() => {
-        if (data) {
-            const { profile } = data;
-            if (profile) {
-                const { profile } = data;
-                setProfile(profile);
-            } else {
-                if (!loading) {
-                    cleanEmailAndCookie()
-                }
-            }
-        }
-    }, [data]);
-
-    const fetchCredit = async () => {
-        setLoadingCredit(true)
-        if (profile) {
-            const { data } = await FetchCredit(profile.document)
-            SetCredit(data.SaldoMonetario)
-        }
-        setLoadingCredit(false)
+  useEffect(() => {
+    if (data) {
+      const { profile } = data;
+      if (profile) {
+        const { profile } = data;
+        setProfile(profile);
+      } else if (!loading) {
+        cleanEmailAndCookie();
+      }
     }
+  }, [data]);
 
-    useEffect(() => {
-        fetchCredit()
-    }, [profile])
+  useEffect(() => {
+    remoteConfig().fetchAndActivate();
+    const response = remoteConfig().getValue('cashback_in_store');
 
-    return (
-        <SafeAreaView flex={1} backgroundColor='white'>
-            <TopBarBackButton
-                loading={loading || loadingCredit}
-                showShadow
-                backButtonPress={() => navigation.goBack()}
+    setCashbackInStore(response.asBoolean());
+  }, []);
+
+  const fetchCredit = async () => {
+    setLoadingCredit(true);
+    if (profile) {
+      const { data } = await FetchCredit(profile.document);
+      SetCredit(data.SaldoMonetario);
+    }
+    setLoadingCredit(false);
+  };
+
+  useEffect(() => {
+    fetchCredit();
+  }, [profile]);
+
+  return (
+    <SafeAreaView flex={1} backgroundColor="white">
+      <TopBarBackButton
+        loading={loading || loadingCredit}
+        showShadow
+        backButtonPress={() => navigation.goBack()}
+      />
+      <Box mx={20} mt="sm">
+        <Box mb="nano">
+          <Typography variant="tituloSessoes">Meus créditos</Typography>
+        </Box>
+        <Typography fontFamily="nunitoRegular" fontSize={14}>
+          Use o crédito na sua próxima compra. Ele aparecerá automaticamente no
+          ato do pagamento.
+        </Typography>
+        <Box mt={20}>
+          <Divider variant="fullWidth" />
+          <Box py={20} flexDirection="row" justifyContent="space-between">
+            <Typography variant="subtituloSessoes">
+              Créditos e cashback
+            </Typography>
+            <PriceCustom
+              fontFamily="nunitoBold"
+              num={credit}
+              sizeDecimal={13}
+              sizeInterger={20}
             />
-            <Box mx={20} mt='sm'>
-                <Box mb='nano'>
-                    <Typography variant='tituloSessoes'>Meus créditos</Typography>
-                </Box>
-                <Typography fontFamily='nunitoRegular' fontSize={14}>
-                    Use o crédito na sua próxima compra. Ele aparecerá automaticamente no
-                    ato do pagamento.
-                </Typography>
-                <Box mt={20}>
-                    <Divider variant='fullWidth' />
-                    <Box py={20} flexDirection='row' justifyContent='space-between'>
-                        <Typography variant='subtituloSessoes'>
-                            Créditos e cashback
-                        </Typography>
-                        <PriceCustom
-                            fontFamily='nunitoBold'
-                            num={credit}
-                            sizeDecimal={13}
-                            sizeInterger={20}
-                        />
-                    </Box>
-                    <Divider variant='fullWidth' />
-                    <Box flexDirection="row" mt="xxs">
-                        <Button flexDirection="row" onPress={() => { navigation.navigate('Cashback', { credits: credit }) }}>
-                            <>
-                                <Icon name="Cashback" size={20} color="preto" mr="xxxs" />
+          </Box>
+          <Divider variant="fullWidth" />
+          {cashbackInStore && (
+            <Box flexDirection="row" mt="xxs">
+              <Button
+                flexDirection="row"
+                onPress={() => {
+                  navigation.navigate('Cashback', { credits: credit });
+                }}
+              >
+                <>
+                  <Icon name="Cashback" size={20} color="preto" mr="xxxs" />
 
-                                <Typography
-                                    fontFamily='nunitoBold'
-                                    fontSize={16}
-                                >
-                                    Cashback em Lojas
-                                </Typography>
-                            </>
-                        </Button>
-                    </Box>
-                </Box>
+                  <Typography fontFamily="nunitoBold" fontSize={16}>
+                    Cashback em Lojas
+                  </Typography>
+                </>
+              </Button>
             </Box>
-        </SafeAreaView>
-    )
-}
+          )}
+        </Box>
+      </Box>
+    </SafeAreaView>
+  );
+};
