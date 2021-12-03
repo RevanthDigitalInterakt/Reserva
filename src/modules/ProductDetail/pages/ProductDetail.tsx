9aab346fdc950643da59a7a71f9e569d7a4a2f53
Tiago Rodrigues <tiiago.santosr@gmail.com>
2021-12-03 15:00:35 -0300

    fix: clickable image

--- src/modules/ProductDetail/pages/ProductDetail.tsx
+++ src/modules/ProductDetail/pages/ProductDetail.tsx
@@ -255,10 +255,13 @@ export const ProductDetail: React.FC<Props> = ({
       const colorItemId = product.items.find((item) => item.itemId == route.params?.itemId)?.variations?.find((x) => x.name == "VALOR_HEX_ORIGINAL")?.values
 
       setColorFilters(colorList);
-
       // set initial selected color
       if (route.params?.itemId) {
-        setSelectedColor(colorList ? colorItemId[0] : '');
+        if (colorItemId) {
+          setSelectedColor(colorList ? colorItemId[0] : '');
+        } else {
+          setSelectedColor(colorList ? colorList[0] : '');
+        }
       } else {
         setSelectedColor(colorList ? route.params.colorSelected : '');
       }
