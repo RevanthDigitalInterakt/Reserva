52e74ab73b548bccadfedd64a60289a0758f9e47
Tiago Rodrigues <tiiago.santosr@gmail.com>
2021-12-22 10:50:07 -0300

    feat:  link apple

--- src/modules/Update/pages/StoreUpdatePush.ts
+++ src/modules/Update/pages/StoreUpdatePush.ts
@@ -3,7 +3,7 @@ import { Linking, Platform } from 'react-native'
 const StoreUpdatePush = () => {
     Linking.openURL(
         Platform.OS === 'ios' ?
-            'itms-apps://itunes.apple.com/us/app/apple-store/com.globalsys.reserva?mt=8'
+            'itms-apps://itunes.apple.com/app/apple-store/id1566861458'
             :
             'market://details?id=com.usereserva')
 
