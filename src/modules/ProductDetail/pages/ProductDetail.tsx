5f3541eb2121b5620858cf2b323a275e1970d7f5
Tiago Santos Rodrigues <tiago.rodrigues@globalsys.com.br>
2022-01-27 14:10:32 -0300

    feat: product description

--- src/modules/ProductDetail/pages/ProductDetail.tsx
+++ src/modules/ProductDetail/pages/ProductDetail.tsx
@@ -105,6 +105,7 @@ type Facets = {
 };
 
 type Variant = {
+  ean: string;
   itemId: string;
   images: {
     imageUrl: string;
@@ -127,7 +128,11 @@ type Specification = {
   field: Field;
   values: Field[];
 };
-
+type Properties = {
+  name: string;
+  originalName: string;
+  values: string[];
+}
 type Product = {
   categoryTree: any[]; // doesnt matter
   productId: string;
@@ -138,6 +143,7 @@ type Product = {
     sellingPrice: Price;
     listPrice: Price;
   };
+  properties: Properties[];
   items: Variant[];
   description: string;
 };
@@ -1436,18 +1442,11 @@ export const ProductDetail: React.FC<Props> = ({
                     ))}
                   <Divider variant="fullWidth" my="xs" />
 
-                  <Box>
-                    <ExpansePanel
-                      style={{
-                        fontFamily: 'reservaSerifRegular',
-                        fontSize: 20,
-                      }}
-                      information={{
-                        title: 'Sobre este produto',
-                        content: product.description || '',
-                      }}
-                    />
-                  </Box>
+                  <ExpandProductDescription
+                    description={product.description || ''}
+                    composition={product.properties[0].values[0]}
+                    codeProduct={product.items.find((x) => x.itemId === selectedVariant?.itemId)?.ean || ''}
+                  />
 
                   <Divider variant="fullWidth" my="xs" />
                   <Box mb="xxxs">
@@ -1571,3 +1570,84 @@ export const ProductDetail: React.FC<Props> = ({
     </SafeAreaView>
   );
 };
+
+interface IExpandProductDescription {
+  description: string;
+  composition: string;
+  codeProduct: string;
+}
+const ExpandProductDescription = ({ description, composition, codeProduct }: IExpandProductDescription) => {
+  const [showDescription, setShowDescription] = useState(false);
+  return (
+    <Box>
+
+      <Button
+        variant='semBorda'
+        marginTop='xxxs'
+        onPress={() => setShowDescription(!showDescription)}
+        flexDirection="row"
+      >
+        <>
+          {showDescription ?
+            <Box alignSelf="center" paddingRight='quarck' paddingLeft="quarck">
+              <Icon name='Subtraction' color='fullBlack' size={20} />
+            </Box>
+            :
+            <Box alignSelf="center" paddingRight='nano'>
+              <Icon name='Add' color='fullBlack' size={20} />
+            </Box>
+          }
+          <Box flex={1}>
+            <Typography fontFamily="reservaSerifRegular" fontSize={20}>
+              Sobre este produto
+            </Typography>
+          </Box>
+        </>
+      </Button>
+      {showDescription && (
+        <>
+          <ProductDescription
+            title='Detalhes do Produto'
+            description={description}
+          />
+          <ProductDescription
+            title='Composição'
+            description={composition}
+          />
+          <ProductDescription
+            title='Código do Produto'
+            description={`Ref: ${codeProduct}`}
+          />
+        </>
+      )
+      }
+    </Box>
+  );
+}
+interface IProductDescription {
+  title: string;
+  description: string;
+}
+
+const ProductDescription: React.FC<IProductDescription> = ({ title, description }) => {
+  return (
+    <Box>
+      <Box mt="xxs">
+        <Typography
+          fontFamily='nunitoBold'
+          fontSize={15}
+        >
+          {title}
+        </Typography>
+        <Box mt="nano">
+          <Typography
+            fontFamily='nunitoRegular'
+            fontSize={13}
+          >
+            {description}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
