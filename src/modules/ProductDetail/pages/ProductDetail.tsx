1eb6a4647b8798fbc2e000a30195b6101b11bd57
Tiago Santos Rodrigues <tiago.rodrigues@globalsys.com.br>
2022-01-27 14:53:09 -0300

    feat: refactor code

--- src/modules/ProductDetail/pages/ProductDetail.tsx
+++ src/modules/ProductDetail/pages/ProductDetail.tsx
@@ -25,6 +25,8 @@ import {
 } from 'reserva-ui';
 import { TopBarDefaultBackButton } from '../../Menu/components/TopBarDefaultBackButton';
 import { ModalBag } from '../components/ModalBag';
+import { ExpandProductDescription } from '../components/ExpandProductDescription';
+
 import * as Yup from 'yup';
 import Share from 'react-native-share';
 
@@ -1570,84 +1572,3 @@ export const ProductDetail: React.FC<Props> = ({
     </SafeAreaView>
   );
 };
-
-interface IExpandProductDescription {
-  description: string;
-  composition: string;
-  codeProduct: string;
-}
-const ExpandProductDescription = ({ description, composition, codeProduct }: IExpandProductDescription) => {
-  const [showDescription, setShowDescription] = useState(false);
-  return (
-    <Box>
-
-      <Button
-        variant='semBorda'
-        marginTop='xxxs'
-        onPress={() => setShowDescription(!showDescription)}
-        flexDirection="row"
-      >
-        <>
-          {showDescription ?
-            <Box alignSelf="center" paddingRight='quarck' paddingLeft="quarck">
-              <Icon name='Subtraction' color='fullBlack' size={20} />
-            </Box>
-            :
-            <Box alignSelf="center" paddingRight='nano'>
-              <Icon name='Add' color='fullBlack' size={20} />
-            </Box>
-          }
-          <Box flex={1}>
-            <Typography fontFamily="reservaSerifRegular" fontSize={20}>
-              Sobre este produto
-            </Typography>
-          </Box>
-        </>
-      </Button>
-      {showDescription && (
-        <>
-          <ProductDescription
-            title='Detalhes do Produto'
-            description={description}
-          />
-          <ProductDescription
-            title='Composição'
-            description={composition}
-          />
-          <ProductDescription
-            title='Código do Produto'
-            description={`Ref: ${codeProduct}`}
-          />
-        </>
-      )
-      }
-    </Box>
-  );
-}
-interface IProductDescription {
-  title: string;
-  description: string;
-}
-
-const ProductDescription: React.FC<IProductDescription> = ({ title, description }) => {
-  return (
-    <Box>
-      <Box mt="xxs">
-        <Typography
-          fontFamily='nunitoBold'
-          fontSize={15}
-        >
-          {title}
-        </Typography>
-        <Box mt="nano">
-          <Typography
-            fontFamily='nunitoRegular'
-            fontSize={13}
-          >
-            {description}
-          </Typography>
-        </Box>
-      </Box>
-    </Box>
-  );
-}
