d19696ef04330af775f6fa4ffba0d6f4f66b1858
Tiago Santos Rodrigues <tiago.rodrigues@globalsys.com.br>
2022-02-02 16:27:30 -0300

    fix: push notification image

--- ios/OneSignalNotificationServiceExtension/NotificationService.m
+++ ios/OneSignalNotificationServiceExtension/NotificationService.m
@@ -1,15 +1,11 @@
-//
-//  NotificationService.m
-//  OneSignalNotificationServiceExtension
-//
-//  Created by Danilo Machado de Sousa on 12/01/22.
-//
+#import <OneSignal/OneSignal.h>
 
 #import "NotificationService.h"
 
 @interface NotificationService ()
 
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNNotificationRequest *receivedRequest;
 @property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
 
 @end
@@ -17,18 +13,31 @@
 @implementation NotificationService
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    self.receivedRequest = request;
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
-
-    // Modify the notification content here...
-    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ ", self.bestAttemptContent.title];
-
+    
+    [OneSignal didReceiveNotificationExtensionRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+    
+    // DEBUGGING: Uncomment the 2 lines below and comment out the one above to ensure this extension is excuting
+    //            Note, this extension only runs when mutable-content is set
+    //            Setting an attachment or action buttons automatically adds this
+    // NSLog(@"Running NotificationServiceExtension");
+    // self.bestAttemptContent.body = [@"[Modified] " stringByAppendingString:self.bestAttemptContent.body];
+    
+  NSUserDefaults *userDefault = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.globalsys.reserva.onesignal"];
+    NSLog(@"NSE player_id: %@", [userDefault  stringForKey:@"GT_PLAYER_ID"]);
+    NSLog(@"NSE app_id: %@", [userDefault  stringForKey:@"GT_APP_ID"]);
+    
     self.contentHandler(self.bestAttemptContent);
 }
 
 - (void)serviceExtensionTimeWillExpire {
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    
+    [OneSignal serviceExtensionTimeWillExpireRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+    
     self.contentHandler(self.bestAttemptContent);
 }
 
