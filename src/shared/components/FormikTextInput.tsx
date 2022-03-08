19bdbd4149440ca9c9373b27feddd2a641a18464
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2022-03-08 14:40:58 -0300

    fix: input-label

--- src/shared/components/FormikTextInput.tsx
+++ src/shared/components/FormikTextInput.tsx
@@ -35,7 +35,7 @@ export const FormikTextInput = ({
   const [labelDisplay, setLabelDisplay] = useState(label);
 
   const changeLabel = async () => {
-    !values[field] ? setLabelDisplay(null) : setLabelDisplay(label);
+    !values[field] ? setLabelDisplay(undefined) : setLabelDisplay(label);
   };
 
   useEffect(() => {
@@ -45,7 +45,7 @@ export const FormikTextInput = ({
   return (
     <>
       <TextField
-        label={labelDisplay}
+        label={label}
         textAlignVertical={textAlignVertical}
         fontFamily="nunitoRegular"
         secureTextEntry={secureTextEntry}
