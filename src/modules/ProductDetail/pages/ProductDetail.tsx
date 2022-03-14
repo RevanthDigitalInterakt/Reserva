c4532f2e46bdebc3f765be1f2c3ede5b3626d550
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2022-03-14 11:41:09 -0300

    fix: share url

--- src/modules/ProductDetail/pages/ProductDetail.tsx
+++ src/modules/ProductDetail/pages/ProductDetail.tsx
@@ -648,7 +648,7 @@ export const ProductDetail: React.FC<Props> = ({
     const options = {
       message: 'Olha o que acabei de encontrar na Reserva: \n',
       title: 'Compartilhar',
-      url: `https://opencashback--applojausereserva.myvtex.com${path}?skuId=${selectedVariant.itemId}`,
+      url: `https://www.usereserva.com${path}?skuId=${selectedVariant.itemId}`,
     };
 
     Share.open(options);
