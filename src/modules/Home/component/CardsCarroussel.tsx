417a20dcfce058badd8fc7c2aef030aef3990399
Tiago Santos Rodrigues <tiago.rodrigues@globalsys.com.br>
2022-03-16 15:20:21 -0300

    feat: mini carousel

--- src/modules/Home/component/CardsCarroussel.tsx
+++ src/modules/Home/component/CardsCarroussel.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
-import { Dimensions, FlatList, Animated } from 'react-native';
+import { Dimensions, FlatList, Animated, View, StyleSheet, useWindowDimensions } from 'react-native';
 import { Box, Button, Image } from 'reserva-ui';
 import { Carrousel, CarrouselCard } from 'src/graphql/homePage/HomeQuery';
 
 const cardWidth = Dimensions.get('window').width * 0.85;
 const cardPadding = Dimensions.get('window').width * 0.15 * 0.5;
 const DEVICE_WIDTH = Dimensions.get('window').width;
+
 interface CardsCarrouselProps {
   carrousel: Carrousel;
 }
@@ -18,16 +19,25 @@ export const CardsCarrousel: React.FC<CardsCarrouselProps> = ({
   console.log('carrousel', carrousel);
   const myCards = carrousel.itemsCollection.items;
   const scrollX = useRef(new Animated.Value(0)).current
+
+  const { width } = useWindowDimensions();
+  const defaultProps = {
+    dotSize: 24,
+    borderPadding: -5,
+  };
+  const inputRange = [-width, -48, width];
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: [
+      -30,
+      0,
+      30,
+    ],
+  });
+
   return (
-    <Box
-    // marginY={15}
-    >
+    <Box>
       <Box>
-        {/* <Box paddingLeft={15}>
-          <Typography fontFamily="nunitoBold" fontSize={16}>
-            {carrousel.title}
-          </Typography>
-        </Box> */}
         <Animated.FlatList
           onScroll={Animated.event(
             [{ nativeEvent: { contentOffset: { x: scrollX } } }],
@@ -51,7 +61,6 @@ export const CardsCarrousel: React.FC<CardsCarrouselProps> = ({
           // pagingEnabled
           // decelerationRate={0}
           bounces={false}
-          // disableIntervalMomentum
           renderItem={({ item, index }) => (
             <Box>
               <Card
@@ -67,50 +76,40 @@ export const CardsCarrousel: React.FC<CardsCarrouselProps> = ({
         />
 
         <Box
-          // marginY={5}
-          marginTop={3}
-          marginBottom={1}
-          flexDirection={'row'}
-          alignSelf={'center'}
+          height={24}
+          flexDirection='row'
+          alignSelf='center'
         >
-          {Array(3).fill(0).map((_, index) => (
-            index != 0
-              ? <Box
-                width={8}
-                height={8}
-                bg="white"
-                borderRadius={'xxxs'}
-                borderColor='#6F6F6F'
-                borderWidth={1}
-                ml={'nano'}
-              />
-              : <Box
-                width={8}
-                height={8}
-                bg="white"
-                borderRadius={'xxxs'}
-                borderColor='#6F6F6F'
-                borderWidth={1}
-              />
-          ))}
           <Animated.View
-            style={{
-              width: 8,
-              height: 8,
-              backgroundColor: '#6F6F6F',
-              borderRadius: 20,
-              position: 'absolute',
-              transform: [{
-                translateX: Animated.divide(scrollX, DEVICE_WIDTH * 0.85 - 100).interpolate({
-                  inputRange: [0, 1],
-                  outputRange: [0, 8 + 5]
-                })
-              }]
-            }}
+            style={[
+              styles.slidingIndicatorStyle,
+              {
+                position: 'absolute',
+                transform: [{ translateX }],
+              },
+            ]}
           />
+          {myCards.map((_item, index) => {
+            return (
+              <Box
+                key={index}
+                justifyContent='center'
+                alignItems='center'
+                width={19}
+              >
+                <Box
+                  borderWidth={1}
+                  width={12}
+                  height={12}
+                  borderRadius={12}
+                  borderColor='#6F6F6F'
+                />
+              </Box>
+            );
+          })}
         </Box>
       </Box>
-    </Box>
+    </Box >
   );
 };
 
@@ -151,7 +150,6 @@ const Card: React.FC<CardProps> = ({
         });
       }
       navigation.navigate('ProductCatalog', {
-        // facetInput,
         referenceId: reference,
       });
     }
@@ -159,44 +157,23 @@ const Card: React.FC<CardProps> = ({
 
   return (
     <Box>
-      {/* <Box> */}
       <Button onPress={handleNavigation}>
         <Image
           autoHeight
           width={DEVICE_WIDTH * 0.85 - 16}
           source={{ uri: image.url }} />
       </Button>
-      {/* <Box
-          style={{ maxWidth: width }}
-          marginLeft={10}
-          marginTop="quarck"
-          marginBottom={5}
-        >
-          <Typography fontFamily="reservaSansBold" fontSize={16}>
-            {name.toUpperCase()}
-          </Typography>
-          <Typography
-            style={{
-              marginLeft: 10,
-              height: (12 + 4) * 3,
-            }}
-            numberOfLines={3}
-            fontFamily="reservaSansRegular"
-            fontSize={12}
-            color="neutroFrio2"
-          >
-            {description}
-          </Typography>
-        </Box>
-      </Box> */}
-      {/* <TouchableOpacity
-        onPress={handleNavigation}
-        style={{ bottom: 0, marginLeft: 10 }}
-      >
-        <Typography fontSize={14} fontFamily="nunitoBold">
-          {referenceLabel}
-        </Typography>
-      </TouchableOpacity> */}
     </Box>
   );
 };
+
+const styles = StyleSheet.create({
+  slidingIndicatorStyle: {
+    backgroundColor: '#6F6F6F',
+    width: 12,
+    height: 12,
+    alignSelf: 'center',
+    zIndex: 2,
+    borderRadius: 12,
+  },
+});
