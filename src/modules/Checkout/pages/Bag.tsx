4915a42c897f46713d02b9e7fd85e9f5173f0c7d
VlademirMelo <vlademir.melo@globalsys.com.br>
2022-05-19 16:38:00 -0300

    feature: code in uppercase format

--- src/modules/Checkout/pages/Bag.tsx
+++ src/modules/Checkout/pages/Bag.tsx
@@ -798,7 +798,7 @@ export const BagScreen = () => {
                 {/* cupom vendedor */}
                 {!!sellerCode && (
                   <CouponBadge
-                    value={`${sellerName} | ${sellerCode}`}
+                    value={`${sellerName} | ${sellerCode.toUpperCase()}`}
                     onPress={async () => {
                       setLoading(true);
                       await removeSellerCoupon(''); // remove passando ''
