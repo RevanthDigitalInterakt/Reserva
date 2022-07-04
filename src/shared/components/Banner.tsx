94098ab39c299388312f7043156ab3c231ddc7d4
Tiago Santos Rodrigues <tiago.rodrigues@globalsys.com.br>
2022-07-04 15:00:21 -0300

    feat: Banner

---
+++ src/shared/components/Banner.tsx
@@ -0,0 +1,16 @@
+import React, { } from 'react';
+import {
+    Box,
+    Image,
+} from '@danilomsou/reserva-ui';
+
+interface IBanner {
+    bannerImage: string;
+}
+export const Banner = ({ bannerImage }: IBanner) => {
+    return (
+        <Box>
+            <Image height={200} source={bannerImage} width={1 / 1} />
+        </Box>
+    );
+}
