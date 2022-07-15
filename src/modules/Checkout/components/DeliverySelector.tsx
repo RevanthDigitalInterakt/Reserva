ead528df0b93e17a058dd5b5fc4de9127ed15134
Cleiber Reis <cleiber.reis@globalsys.com.br>
2022-07-15 18:06:17 -0300

    feat: change address checkout

--- src/modules/Checkout/components/DeliverySelector.tsx
+++ src/modules/Checkout/components/DeliverySelector.tsx
@@ -1,117 +1,125 @@
 import * as React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Typography, Box, Button, Icon } from '@danilomsou/reserva-ui';
+import { Typography, Box, Button, Icon, theme } from '@danilomsou/reserva-ui';
 
 export interface Delivery {
-    id?: string;
-    name?: string;
-    price?: number;
-    shippingEstimate?: string;
+  id?: string;
+  name?: string;
+  price?: number;
+  shippingEstimate?: string;
 }
 interface IDeliverySelector {
-    select?: () => void;
-    deliveryData: Delivery;
-    selected: boolean;
-    disabled?: boolean;
+  select?: () => void;
+  deliveryData: Delivery;
+  selected: boolean;
+  disabled?: boolean;
 }
 
 const DeliverySelector = ({
-    selected,
-    select,
-    deliveryData,
-    disabled
+  selected,
+  select,
+  deliveryData,
+  disabled,
 }: IDeliverySelector) => {
-    const { name, price, shippingEstimate, id } = deliveryData;
-    return (
-        <>
-            <TouchableOpacity
-                disabled={disabled}
-                onPress={select}>
+  const { name, price, shippingEstimate, id } = deliveryData;
+  return (
+    <>
+      <TouchableOpacity disabled={disabled} onPress={select}>
+        <Box
+          bg="white"
+          borderWidth="hairline"
+          borderColor="preto"
+          width="100%"
+          height={84}
+          flexDirection="row"
+          p="nano"
+          alignItems="center"
+        >
+          <Box
+            height={50}
+            width={50}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box m={'nano'} alignItems="center">
+              <Box width="10%">
                 <Box
-                    bg="white"
-                    borderWidth="hairline"
-                    borderColor={'divider'}
-                    width="100%"
-                    height={100}
-                    flexDirection="row"
-                    p="nano"
-                    mt={'nano'}
-                    alignItems="center"
+                  height={20}
+                  width={20}
+                  borderRadius="infinity"
+                  borderWidth="thin"
+                  alignItems="center"
+                  justifyContent="center"
                 >
+                  {selected && (
                     <Box
-                        height={50}
-                        width={50}
-                        justifyContent="center"
-                        alignItems="center"
-                    >
-                        <Box m={'nano'} alignItems="center">
-                            <Box width="10%">
-                                <Box
-                                    height={20}
-                                    width={20}
-                                    borderRadius="infinity"
-                                    borderWidth="thin"
-                                    alignItems="center"
-                                    justifyContent="center"
-                                >
-                                    {selected &&
-                                        <Box
-                                            height={10}
-                                            width={10}
-                                            borderRadius="nano"
-                                            bg="preto"
-                                        />
-                                    }
-                                </Box>
-                            </Box>
-                        </Box>
-                    </Box>
-                    <Box
-                        alignContent={"center"}
-                        flex={1}
-                        width="70%"
-                        marginX="micro"
-                        borderRightWidth="hairline"
-                        borderColor="divider"
-                    >
-                        <Typography fontFamily="reservaSerifRegular" fontSize={16}>
-                            {name}
-                        </Typography>
-                        <Typography
-                            style={{ flexWrap: 'wrap' }}
-                            fontFamily="nunitoRegular"
-                            fontSize={12}
-                        >
-                            Em até {shippingEstimate?.split('bd')[0]} dias úteis</Typography>
-                    </Box>
-                    {price && price > 0 ?
-                        <Box width="20%" alignItems="center">
-                            <Typography>
-                                R$ {(price / 100).toFixed(2).replace(".", ",")}
-                            </Typography>
-                        </Box>
-                        :
-                        <Box
-                            borderRadius="infinity"
-                            bg="verdeSucesso"
-                            borderColor="verdeSucesso"
-                            px="micro"
-                            py="nano"
-                            alignItems="center"
-                        >
-                            <Typography
-                                color="white"
-                                fontFamily="reservaSansMedium"
-                                fontSize={11}
-                            >
-                                GRÁTIS
-                            </Typography>
-                        </Box>
-                    }
+                      height={10}
+                      width={10}
+                      borderRadius="nano"
+                      bg="preto"
+                    />
+                  )}
                 </Box>
-            </TouchableOpacity>
-        </>
-    );
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            alignContent={'center'}
+            flex={1}
+            marginX="micro"
+            borderRightWidth="hairline"
+            borderColor="divider"
+            marginLeft="nano"
+          >
+            <Typography
+              fontFamily="reservaSerifRegular"
+              fontSize={16}
+              lineHeight={21}
+            >
+              {name}
+            </Typography>
+            <Typography
+              style={{ flexWrap: 'wrap' }}
+              fontFamily="nunitoRegular"
+              fontSize={13}
+              lineHeight={16}
+            >
+              Em até {shippingEstimate?.split('bd')[0]} dias úteis
+            </Typography>
+          </Box>
+          <Box width="25%" alignItems="center">
+            {price && price > 0 ? (
+              <Box alignItems="center">
+                <Typography>
+                  R$ {(price / 100).toFixed(2).replace('.', ',')}
+                </Typography>
+              </Box>
+            ) : (
+              <Box
+                borderRadius="infinity"
+                bg="verdeSucesso"
+                borderColor="verdeSucesso"
+                px="nano"
+                py="nano"
+                alignItems="center"
+                width={74}
+                height={31}
+              >
+                <Typography
+                  color="white"
+                  fontFamily="nunitoRegular"
+                  fontSize={11}
+                  letterSpacing={1.6}
+                >
+                  GRÁTIS
+                </Typography>
+              </Box>
+            )}
+          </Box>
+        </Box>
+      </TouchableOpacity>
+    </>
+  );
 };
 
 export default DeliverySelector;
