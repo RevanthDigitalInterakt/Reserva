b104b94ad205c75a989fbbb58c8b8b0c0eac222d
Danilo Machado de Sousa <danilo.sousa@globalsys.com.br>
2022-08-05 11:08:18 -0300

    feat: add push io

---
+++ ios/Framework/PIOContentExtension.framework/Versions/A/Headers/PIOContentExtension.h
@@ -0,0 +1,16 @@
+//
+//  PIOContentExtension.h
+//  PIOContentExtension
+//
+//  Copyright (c) 2018 Oracle Inc. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+#import <PIOContentExtension/PIONotificationViewController.h>
+
+//! Project version number for PIOMediaAttachmentExtension.
+FOUNDATION_EXPORT double PIOContentExtensionVersionNumber;
+
+//! Project version string for PIOMediaAttachmentExtension.
+FOUNDATION_EXPORT const unsigned char  PIOContentExtensionVersionString[];
+
---
+++ ios/XCFramework/PIOContentExtension.xcframework/ios-arm64_x86_64-simulator/PIOContentExtension.framework/Headers/PIONotificationViewController.h
@@ -0,0 +1,15 @@
+//
+//  NotificationViewController.h
+//  CarouselPush
+//
+//  Created by Sahil Wasan on 17/06/19.
+//  Copyright © 2019 Sahil Wasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface PIONotificationViewController : UIViewController<UNNotificationContentExtension>
+
+@end
