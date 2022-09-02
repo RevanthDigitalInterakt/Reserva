48385fbc4496c3f5455165a695ae38c7fce6d51f
danilomsou <danilo.sousa@globalsys.com.br>
2022-09-02 11:32:27 -0300

    chore: bump version 1.8.11

--- src/modules/Checkout/pages/WebviewCheckout.tsx
+++ src/modules/Checkout/pages/WebviewCheckout.tsx
@@ -6,6 +6,7 @@ import appsFlyer from 'react-native-appsflyer';
 import * as StoreReview from 'react-native-store-review';
 import { WebView } from 'react-native-webview';
 import { Box, Button } from '@danilomsou/reserva-ui';
+import analytics from '@react-native-firebase/analytics';
 import { loadingSpinner } from '@danilomsou/reserva-ui/src/assets/animations';
 import { useCart } from '../../../context/CartContext';
 import { TopBarBackButton } from '../../Menu/components/TopBarBackButton';
@@ -33,10 +34,12 @@ const Checkout: React.FC<{}> = () => {
     const check = navState.includes('/checkout/orderPlaced');
     if (check) {
       if (orderForm) {
-        const revenue_total = orderForm.totalizers.find((item) => item.id === 'Items')?.value;
+        const revenue_total = orderForm.totalizers.find(
+          (item) => item.id === 'Items'
+        )?.value;
         let af_revenue = '0';
 
-        if(revenue_total) {
+        if (revenue_total) {
           af_revenue = (revenue_total / 100).toFixed(2);
         }
         appsFlyer.logEvent('af_purchase', {
@@ -50,6 +53,28 @@ const Checkout: React.FC<{}> = () => {
           af_quantity: orderForm.items.map((item) => item.quantity),
           af_order_id: orderForm.orderFormId,
           // af_receipt_id: orderForm.paymentData,
+        });
+
+        analytics().logPurchase({
+          affiliation: 'APP',
+          coupon: 'coupon',
+          currency: 'BRL',
+          items: orderForm.items.map((item) => ({
+            price: item.price / 100,
+            item_id: item.productId,
+            quantity: item.quantity,
+            item_name: item.name,
+            item_variant: item.skuName,
+            item_category: Object.values(item.productCategories).join('|'),
+          })),
+          shipping:
+            (orderForm.totalizers.find((x) => x.name === 'Shipping')?.value ||
+              0) / 100,
+          tax:
+            (orderForm.paymentData.payments[0].merchantSellerPayments[0]
+              .interestRate || 0) / 100,
+          transaction_id: '',
+          value: orderForm.value / 100,
         });
       }
       orderform();
