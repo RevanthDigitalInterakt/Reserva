f60242b91ed6f9950d6c4facc6579c1e34404360
Thulio.Gamarano <thulio.gamarano@globalsys.com.br>
2022-09-20 15:21:10 -0300

    flag mkt menu

--- src/graphql/categories/categoriesQuery.ts
+++ src/graphql/categories/categoriesQuery.ts
@@ -3,16 +3,16 @@
 import { gql } from "@apollo/client";
 
 export type CategoryQuery = {
-    id: number;
-    href: string;
-    slug: string;
-    name: string;
-    titleTag: string;
-    hasChildren: boolean;
-    metaTagDescription: string;
-    children: [CategoryQuery]
-    highlight?: boolean;
-    opened?: boolean;
+  id: number;
+  href: string;
+  slug: string;
+  name: string;
+  titleTag: string;
+  hasChildren: boolean;
+  metaTagDescription: string;
+  children: [CategoryQuery]
+  highlight?: boolean;
+  opened?: boolean;
 }
 
 export const categoriesQuery = gql`
@@ -21,6 +21,7 @@ export const categoriesQuery = gql`
       items {
         itemsCollection(limit: 100) {
           items {
+            mkt
             name
             referenceId
             childCategoryCollection(limit: 100) {
