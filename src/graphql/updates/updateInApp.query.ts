649bf8a4262e1ea394ecf1f6aa1042e4d8acaf33
danilomsou <danilo.sousa@globalsys.com.br>
2022-09-21 14:14:46 -0300

    feat: add update in app

---
+++ src/graphql/updates/updateInApp.query.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const UPDATE_IN_APP_QUERY = gql`
+  query UpdateInApp {
+    updateInApp(id: "5zDaJDuIr9J9Ul0knN01eF") {
+      updateTitle
+      updateDescription
+      updateAllVersions
+      targetVersion
+      onlyPlatform
+      targetVersionInStore
+      updateType
+    }
+  }
+`;
---
+++ src/updates/InAppUpdates/InAppUpdates.tsx
@@ -0,0 +1,32 @@
+import checkVersion from 'react-native-store-version';
+import DeviceInfo from 'react-native-device-info';
+import SpInAppUpdates, { IAUUpdateKind } from 'sp-react-native-in-app-updates';
+
+const IOS_STORE_URL = 'https://apps.apple.com/br/app/reserva/id1566861458';
+const ANDROID_STORE_URL =
+  'https://play.google.com/store/apps/details?id=com.usereserva';
+
+export const haveVersionUpdates = async () => {
+  try {
+    const { remote, local } = await checkVersion({
+      version: DeviceInfo.getVersion(),
+      iosStoreURL: IOS_STORE_URL,
+      androidStoreURL: ANDROID_STORE_URL,
+      country: 'BR',
+    });
+    const inAppUpdates = new SpInAppUpdates(false);
+
+    const response = await inAppUpdates.checkNeedsUpdate({
+      curVersion: local,
+      customVersionComparator: () => {
+        return remote > local ? 1 : -1;
+      },
+    });
+    if (response.shouldUpdate) {
+      inAppUpdates.addStatusUpdateListener();
+    }
+    return response.shouldUpdate;
+  } catch (error) {
+    console.log(error);
+  }
+};
