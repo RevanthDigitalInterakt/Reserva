31b41f8dda28322830b854a874b20029e1abfe52
Lima <joaolima@frwk.com.br>
2022-11-23 10:44:58 -0300

    add regex to validate product url

--- src/config/linking.ts
+++ src/config/linking.ts
@@ -60,7 +60,7 @@ export const linkingConfig: LinkingOptions = {
         return 'usereserva://home-tabs';
       }
 
-      if (url.includes('/p')) {
+      if (/(?:\b\/p\b.?)/gm.test(url)) {
         const productUrl = new URL(url);
 
         if (!productUrl.search.length) {
