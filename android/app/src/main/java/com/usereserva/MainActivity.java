7e54b0756d1273df2f3e91cdfa0e93da593dc0a8
Leonardo Nogueira <nogueirahy@gmail.com>
2023-03-30 17:14:53 +0000

    Merged in fix/issue-react-native-screen (pull request #553)
    fix: ajust onCreate
    
    * fix: ajust onCreate
    
    
    Approved-by: Cleiber Rodrigues Reis
    Approved-by: João Lima
    Approved-by: Pedro Marques
    Approved-by: Marco Souza
    Approved-by: Luan.souza

--- android/app/src/main/java/com/usereserva/MainActivity.java
+++ android/app/src/main/java/com/usereserva/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
       RNBootSplash.init(this);
-      super.onCreate(savedInstanceState);
+      super.onCreate(null);
   }
 
   @Override
