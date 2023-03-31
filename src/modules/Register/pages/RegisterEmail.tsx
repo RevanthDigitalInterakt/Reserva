73232805bf56ed08bdc9e4ac59582d50f839ea68
Marco Souza <marcosouza@frwk.com.br>
2023-03-31 17:14:03 +0000

    Merged in feature/APP2022-899-email-validate (pull request #555)
    Validate email on pre-register screen
    
    * feat: validate email on pre-register
    
    
    Approved-by: Cleiber Rodrigues Reis
    Approved-by: João Lima
    Approved-by: Vinicius Italo
    Approved-by: Leonardo Nogueira

--- src/modules/Register/pages/RegisterEmail.tsx
+++ src/modules/Register/pages/RegisterEmail.tsx
@@ -1,8 +1,8 @@
 import { useMutation } from '@apollo/client';
 import AsyncStorage from '@react-native-community/async-storage';
-import { StackScreenProps } from '@react-navigation/stack';
+import type { StackScreenProps } from '@react-navigation/stack';
 import * as React from 'react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Typography, Box, Button } from '@usereservaapp/reserva-ui';
 import { images } from '../../../assets';
@@ -12,112 +12,97 @@ import { RootStackParamList } from '../../../routes/StackNavigator';
 import UnderlineInput from '../../Login/components/UnderlineInput';
 import HeaderBanner from '../../Forgot/componet/HeaderBanner';
 import { useCart } from '../../../context/CartContext';
+import { validateEmail } from '../../../utils/validateEmail';
 
 export interface RegisterEmailProps
   extends StackScreenProps<RootStackParamList, 'RegisterEmail'> {}
 
 export const RegisterEmail: React.FC<RegisterEmailProps> = ({ navigation }) => {
-  const { cookie, setCookie } = useAuth();
-
-  const [email, setEmail] = useState<string>('');
-  const [showRecoveryPassword, setShowRecoveryPassword] = useState<boolean>(false);
-  const [emailEmpty, setEmailEmpty] = useState<boolean>(true);
-
+  const { setCookie } = useAuth();
   const { verifyEmail } = useCart();
 
-  const [sendEmailVerification, { data, loading }] = useMutation(
-    sendEmailVerificationMutation,
-  );
+  const [email, setEmail] = useState('');
+  const [showRecoveryPassword, setShowRecoveryPassword] = useState(false);
+  const [inputError, setInputError] = useState('');
 
-  const handleEmailAccess = async () => {
+  const [sendEmailVerification] = useMutation(sendEmailVerificationMutation);
+
+  const handleEmailAccess = useCallback(async () => {
+    const validEmail = validateEmail(email);
+
+    if (!validEmail) {
+      setInputError('Por favor, informe um e-mail válido.');
+      return;
+    }
+
     const isEmailAlreadyExist = await verifyEmail(email);
 
     if (isEmailAlreadyExist) {
+      setInputError('E-mail já cadastrado em nosso banco de dados');
       setShowRecoveryPassword(true);
-    } else {
-      sendEmailVerification({
-        variables: {
-          email,
-        },
-      }).then((x) => {
-        setCookie(x?.data?.cookie);
-        AsyncStorage.setItem('@RNAuth:cookie', x?.data?.cookie);
-        navigation.navigate('ConfirmAccessCode', { email });
-      });
+      return;
     }
-  };
 
-  const handleEmailRecovery = () => {
     sendEmailVerification({
-      variables: {
-        email,
-      },
+      variables: { email },
+    }).then((x) => {
+      setCookie(x?.data?.cookie);
+      AsyncStorage.setItem('@RNAuth:cookie', x?.data?.cookie);
+      navigation.navigate('ConfirmAccessCode', { email });
+    });
+  }, [email]);
+
+  const handleEmailRecovery = useCallback(() => {
+    sendEmailVerification({
+      variables: { email },
     }).then((x) => {
       setCookie(x?.data?.cookie);
       AsyncStorage.setItem('@RNAuth:cookie', x?.data?.cookie);
       navigation.navigate('ForgotAccessCode', { email });
     });
-  };
+  }, [email]);
+
+  useEffect(() => {
+    setInputError('');
+    setShowRecoveryPassword(false);
+  }, [email]);
 
   return (
     <SafeAreaView style={{ backgroundColor: 'white' }} flex={1}>
       <HeaderBanner
         imageHeader={images.headerLogin}
-        onClickGoBack={() => {
-          navigation.goBack();
-        }}
+        onClickGoBack={() => navigation.goBack()}
       />
+
       <Box mx={20} mt={13}>
         <Typography fontFamily="reservaSerifRegular" fontSize={22}>
           Cadastre seu e-mail
         </Typography>
+
         <Box mt={27}>
           <Typography fontFamily="nunitoRegular" fontSize={15}>
             Enviaremos um código de confirmação para o e-mail informado.
           </Typography>
         </Box>
+
         <Box mt={6}>
           <UnderlineInput
             accessibilityLabel="register_input_email"
-            onChangeText={(text) => {
-              setEmail(text);
-
-              if (text.length >= 1) {
-                setEmailEmpty(false);
-              } else {
-                setEmailEmpty(true);
-              }
-            }}
+            onChangeText={setEmail}
             placeholder="abcdefg@gmail.com"
             keyboardType="email-address"
-            showError={showRecoveryPassword}
+            showError={!!inputError}
+            errorMsg={inputError}
           />
         </Box>
-        {showRecoveryPassword && (
-          <Typography
-            color="vermelhoAlerta"
-            fontFamily="nunitoRegular"
-            fontSize={13}
-            style={{
-              marginTop: -14,
-            }}
-          >
-            E-mail já cadastrado em nosso banco de dados
-          </Typography>
-        )}
+
         <Button
           mt={37}
-          variant={
-            showRecoveryPassword
-              ? 'primarioEstreitoOutline'
-              : 'primarioEstreito'
-          }
+          variant={showRecoveryPassword ? 'primarioEstreitoOutline' : 'primarioEstreito'}
           title={showRecoveryPassword ? 'RECUPERAR SENHA' : 'CADASTRAR E-MAIL'}
-          onPress={
-            showRecoveryPassword ? handleEmailRecovery : handleEmailAccess
-          }
+          onPress={showRecoveryPassword ? handleEmailRecovery : handleEmailAccess}
           testID="com.usereserva:id/register_button_recover"
-          disabled={emailEmpty}
+          disabled={!email.length}
           inline
         />
       </Box>
