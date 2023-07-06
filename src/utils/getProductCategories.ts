6acaf3ed30a54225fcfc4a6bf3581e0548c2c47c
luan.souza <luan.souza@globalsys.com.br>
2023-07-06 12:11:08 -0300

    update PR comments

--- src/utils/getProductCategories.ts
+++ src/utils/getProductCategories.ts
@@ -1,7 +1,5 @@
 export const getProductCategories = (categoryTree: string[]) => {
   const categories = categoryTree?.map((item) => item.replace(/-+/g, '-')).join('-').toLowerCase();
 
-	console.log('categories', categories);
-
   return categories;
 };
