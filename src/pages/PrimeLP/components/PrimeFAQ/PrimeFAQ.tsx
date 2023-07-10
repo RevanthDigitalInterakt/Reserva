57f445b5e023791095b1912d6cb3afb24ee18760
Augusto Neves <augustoneves@frwk.com.br>
2023-07-10 12:28:46 -0300

    refactor: using primeFaq query to return FAQ data

--- src/pages/PrimeLP/components/PrimeFAQ/PrimeFAQ.tsx
+++ src/pages/PrimeLP/components/PrimeFAQ/PrimeFAQ.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Typography } from '@usereservaapp/reserva-ui';
 import { View } from 'react-native';
 import DropdownItem from '../../../../components/DropdownItem/DropdownItem';
-import { primeFAQData } from './data';
 import { styles } from './PrimeFAQ.styles';
+import { usePrimeFaqQuery } from '../../../../base/graphql/generated';
 
 const Divider = () => <View style={styles.divider} />;
 
 function PrimeFAQ() {
+  const { data } = usePrimeFaqQuery({
+    context: { clientName: 'gateway' },
+    fetchPolicy: 'network-only',
+  });
+
+  const primeFAQInformation = useMemo(() => data?.primeFaq, [data?.primeFaq]);
+
   return (
     <View style={styles.container}>
       <Typography
@@ -18,15 +25,15 @@ function PrimeFAQ() {
         Perguntas Frequentes
       </Typography>
 
-      {primeFAQData?.map((item, index) => (
+      {primeFAQInformation?.map((item, index) => (
         <>
           <DropdownItem
             body={item.body}
             title={item.title}
-            key={item.title}
+            key={item.id}
             justifyText
           />
-          {index !== primeFAQData.length - 1 && <Divider />}
+          {index !== primeFAQInformation.length - 1 && <Divider />}
         </>
       ))}
     </View>
