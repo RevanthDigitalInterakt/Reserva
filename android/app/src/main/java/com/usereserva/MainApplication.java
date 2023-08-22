a2432c7e4e863ec63c5e5abc761d225a8c9b155c
Henrique Pires <henrique.p.espindola@gmail.com>
2023-08-22 14:47:34 -0300

    chore: remove unecessary react video add package

--- android/app/src/main/java/com/usereserva/MainApplication.java
+++ android/app/src/main/java/com/usereserva/MainApplication.java
@@ -41,7 +41,6 @@ public class MainApplication extends Application implements ReactApplication {
       // Packages that cannot be autolinked yet can be added manually here, for
       // example:
       packages.add(new DeepLinkPathPackage());
-          packages.add(new ReactVideoPackage());
       return packages;
     }
 
