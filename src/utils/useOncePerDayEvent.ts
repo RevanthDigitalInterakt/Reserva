4beb985b0114797dfacf8327c3ba98041716d8a3
Cleiber Reis <cleiber.reis@globalsys.com.br>
2023-08-31 17:20:14 -0300

    feat: create statusCart event

---
+++ src/utils/useOncePerDayEvent.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { ExceptionProvider } from '../base/providers/ExceptionProvider';
+import { trackEventDitoStatusCart } from './trackEventDitoStatusCart';
+import { useBagStore } from '../zustand/useBagStore/useBagStore';
+
+export const has24HoursPassed = async (key: string) => {
+  try {
+    const lastEventDate = await AsyncStorage.getItem(key);
+    // const lastEventDate = '2023-08-29T19:10:58.229Z';
+    if (!lastEventDate) {
+      return true;
+    }
+
+    const lastEventTimestamp = new Date(lastEventDate).getTime();
+    const currentTimestamp = new Date().getTime();
+
+    const twentyFourHoursInMs = 24 * 60 * 60 * 1000;
+    return currentTimestamp - lastEventTimestamp >= twentyFourHoursInMs;
+  } catch (error) {
+    ExceptionProvider.captureException(error);
+    return false;
+  }
+};
+
+export const useOncePerDayEvent = (eventKey: string) => {
+  const [canTriggerEvent, setCanTriggerEvent] = useState<boolean>(false);
+  const {
+    clientProfileData,
+    items,
+    appTotalizers,
+  } = useBagStore(['clientProfileData', 'appTotalizers', 'items']);
+
+  useEffect(() => {
+    const checkEventStatus = async () => {
+      const canTrigger = await has24HoursPassed(eventKey);
+      setCanTriggerEvent(canTrigger);
+    };
+
+    checkEventStatus();
+  }, [eventKey]);
+
+  const triggerEvent = async () => {
+    if (canTriggerEvent) {
+      try {
+        await trackEventDitoStatusCart({
+          items,
+          appTotalizers,
+          clientProfileData,
+        });
+        await AsyncStorage.setItem(eventKey, new Date().toISOString());
+      } catch (error) {
+        ExceptionProvider.captureException(error);
+      }
+    }
+  };
+
+  return {
+    canTriggerEvent,
+    setCanTriggerEvent,
+    triggerEvent,
+  };
+};
