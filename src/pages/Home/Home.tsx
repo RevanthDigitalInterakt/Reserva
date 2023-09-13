72331f9566f8a3a7b4d7b9dac94558ff0cc92cf2
Marco <marcoantoniocanhas@gmail.com>
2023-09-13 18:27:07 +0000

    feat: testing popconvert

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@usereservaapp/reserva-ui';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  FlatList, SafeAreaView,
+  FlatList, SafeAreaView, Text, TouchableOpacity, View,
 } from 'react-native';
 import utc from 'dayjs/plugin/utc';
 import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
+import { WebView } from 'react-native-webview';
+import Modal from 'react-native-modal';
 import testProps from '../../utils/testProps';
 import { TopBarDefault } from '../../modules/Menu/components/TopBarDefault';
 import HomeCarousels from './components/HomeCarousels';
@@ -19,6 +21,9 @@ import ModalSignUpComplete from '../../components/ModalSignUpComplete';
 import { useHomeStore } from '../../zustand/useHomeStore';
 import WithoutInternet from '../../components/WithoutInternet';
 import { useConnectivityStore } from '../../zustand/useConnectivityStore';
+import { COLORS } from '../../base/styles/colors';
+import configDeviceSizes from '../../utils/configDeviceSizes';
+import { platformType } from '../../utils/platformType';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -27,6 +32,8 @@ function Home() {
   const { onLoad, medias, loaded } = useHomeStore(['onLoad', 'medias', 'loaded']);
   const { showModalSignUpComplete } = useAuthModalStore(['showModalSignUpComplete']);
   const { isConnected } = useConnectivityStore(['isConnected']);
+
+  const [isRoletaVisible, setIsRoletaVisible] = useState(false);
 
   useEffect(() => {
     if (isConnected && !loaded) {
@@ -56,6 +63,25 @@ function Home() {
         <FlatList
           ListHeaderComponent={(
             <Box style={{ overflow: 'hidden' }}>
+              <View
+                style={{
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  padding: 20,
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => setIsRoletaVisible(true)}
+                  style={{
+                    backgroundColor: COLORS.RED,
+                    padding: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  <Text style={{ color: '#fff' }}>ABRIR MODAL ROLETA</Text>
+                </TouchableOpacity>
+              </View>
+
               <HomeCountDown />
 
               <HomeCarousels />
@@ -76,6 +102,41 @@ function Home() {
           )}
         />
       </SafeAreaView>
+
+      <Modal
+        isVisible={isRoletaVisible}
+        style={{
+          height: configDeviceSizes.DEVICE_HEIGHT * 0.9,
+        }}
+      >
+        <WebView
+          source={{ uri: `https://www.usereserva.com/files/popconvert.html?${new Date().getTime()}` }}
+          style={{
+            width: '100%',
+            height: configDeviceSizes.DEVICE_HEIGHT * 0.9,
+            backgroundColor: '#fff',
+          }}
+          cacheEnabled={false}
+          cacheMode="LOAD_NO_CACHE"
+          onError={(l) => console.log('onError', l)}
+          onHttpError={(l) => console.log('onHttpError', l)}
+          onMessage={(l) => console.log('onMessage', l)}
+        />
+
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            right: 10,
+            top: platformType.IOS ? 20 : 10,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+            backgroundColor: COLORS.RED,
+          }}
+          onPress={() => setIsRoletaVisible(false)}
+        >
+          <Text style={{ fontSize: 12, color: '#fff' }}>Fechar</Text>
+        </TouchableOpacity>
+      </Modal>
 
       {!!showModalSignUpComplete && <ModalSignUpComplete />}
     </Box>
