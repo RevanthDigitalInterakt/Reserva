f25d089d8041f8dd21208e67a8d6461873eebc0f
luan.souza <luan.souza@globalsys.com.br>
2023-09-18 00:43:15 -0300

    update styles

--- src/pages/Bag/components/Coupon/index.tsx
+++ src/pages/Bag/components/Coupon/index.tsx
@@ -30,8 +30,6 @@ export default function CouponComponent() {
     }));
   }, []);
 
-  console.log('asdadsa', marketingData.itemsWithCouponDiscount);
-
   const handleAddSellerCoupon = useCallback(async () => {
     setSellerCouponError(false);
 
