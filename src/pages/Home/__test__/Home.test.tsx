2850be8742b7df676ba079bc3efc132554561481
Henrique Pires <henrique.p.espindola@gmail.com>
2023-09-20 14:38:50 -0300

    fix: lint

--- src/pages/Home/__test__/Home.test.tsx
+++ src/pages/Home/__test__/Home.test.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { render, screen } from "@testing-library/react-native";
-import { ThemeProvider } from "styled-components/native";
-import { theme } from "@usereservaapp/reserva-ui";
-import { MockedProvider } from "@apollo/client/testing";
-import Home from "../Home";
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from '@usereservaapp/reserva-ui';
+import { MockedProvider } from '@apollo/client/testing';
+import Home from '../Home';
 
 import {
   mockHomeCarouselQuery,
   mockHomeConfigQuery,
   mockHomeCountdownQuery,
   mockHomeMediasQuery,
-} from "./Home.mock";
+} from './Home.mock';
 
 const Component = (
   <ThemeProvider theme={theme}>
@@ -28,33 +28,33 @@ const Component = (
   </ThemeProvider>
 );
 
-jest.mock("react-native-share", () => ({
+jest.mock('react-native-share', () => ({
   open: jest.fn(() => Promise.resolve()),
 }));
 
-jest.mock("../../../hooks/usePrimeInfo", () => ({
+jest.mock('../../../hooks/usePrimeInfo', () => ({
   usePrimeInfo: () => ({}),
 }));
 
-jest.mock("react-native-background-timer", () => ({
+jest.mock('react-native-background-timer', () => ({
   stopBackgroundTimer: jest.fn(),
   runBackgroundTimer: jest.fn(),
 }));
 
-jest.mock("../../../zustand/useApolloFetchPolicyStore", () => ({
+jest.mock('../../../zustand/useApolloFetchPolicyStore', () => ({
   useApolloFetchPolicyStore: () => ({
     initialized: true,
     getFetchPolicyPerKey: () => 0,
   }),
 }));
 
-jest.mock("../../../zustand/useConnectivityStore", () => ({
+jest.mock('../../../zustand/useConnectivityStore', () => ({
   useConnectivityStore: () => ({
     isConnected: true,
   }),
 }));
 
-jest.mock("../../../zustand/useHomeStore", () => ({
+jest.mock('../../../zustand/useHomeStore', () => ({
   useHomeStore: () => ({
     onLoad: () => {},
     medias: mockHomeMediasQuery.result.data.homeMedias,
@@ -62,13 +62,13 @@ jest.mock("../../../zustand/useHomeStore", () => ({
   }),
 }));
 
-jest.mock("../../../zustand/useAuth/useAuthStore", () => ({
+jest.mock('../../../zustand/useAuth/useAuthStore', () => ({
   useAuthStore: () => ({
     initialized: true,
   }),
 }));
 
-describe("Home", () => {
+describe('Home', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -77,23 +77,23 @@ describe("Home", () => {
     jest.clearAllTimers();
   });
 
-  it("renders correctly", () => {
+  it('renders correctly', () => {
     render(Component);
     expect(screen).toMatchSnapshot();
   });
 
-  it("should render flatlist correctly", () => {
+  it('should render flatlist correctly', () => {
     render(Component);
     const card = screen.getByTestId(
-      "com.usereserva:id/home_count_down_container"
+      'com.usereserva:id/home_count_down_container',
     );
     expect(card).toBeOnTheScreen();
   });
 
-  it("should render banner properly", () => {
+  it('should render banner properly', () => {
     render(Component);
     const card = screen.getByTestId(
-      "com.usereserva:id/banner_button_collection:1587"
+      'com.usereserva:id/banner_button_collection:1587',
     );
     expect(card).toBeOnTheScreen();
   });
