a3bdf2731f15270ee882ea6ec8c75a252ad9cbd0
Henrique Pires <henrique.p.espindola@gmail.com>
2023-09-20 11:29:59 -0300

    feature: new home with border radius

--- src/pages/Home/components/HomeMainCarousel/HomeMainCarousel.tsx
+++ src/pages/Home/components/HomeMainCarousel/HomeMainCarousel.tsx
@@ -1,21 +1,22 @@
-import React, {
-  useCallback, useMemo, useRef, useState,
-} from 'react';
-import { useNavigation } from '@react-navigation/native';
-import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
-import { Pressable, View } from 'react-native';
-import { Box } from '@usereservaapp/reserva-ui';
-import { useSharedValue } from 'react-native-reanimated';
-import type { HomeCarouselItemOutput, HomeCarouselOutput } from '../../../../base/graphql/generated';
-import testProps from '../../../../utils/testProps';
-import configDeviceSizes from '../../../../utils/configDeviceSizes';
-import ImageComponent from '../../../../components/ImageComponent/ImageComponent';
-import { COLORS } from '../../../../base/styles/colors';
-import { styles } from './HomeMainCarousel.styles';
-import CarrouselScrollIndicator from '../../../../modules/Home/component/CarouselScrollIndicator';
-import CarouselPaginationItem from '../../../../components/CarouselPaginationItem';
-import { useIsTester } from '../../../../hooks/useIsTester';
-import { useRemoteConfig } from '../../../../hooks/useRemoteConfig';
+import React, { useCallback, useMemo, useRef, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
+import { Pressable, View } from "react-native";
+import { Box } from "@usereservaapp/reserva-ui";
+import { useSharedValue } from "react-native-reanimated";
+import type {
+  HomeCarouselItemOutput,
+  HomeCarouselOutput,
+} from "../../../../base/graphql/generated";
+import testProps from "../../../../utils/testProps";
+import configDeviceSizes from "../../../../utils/configDeviceSizes";
+import ImageComponent from "../../../../components/ImageComponent/ImageComponent";
+import { COLORS } from "../../../../base/styles/colors";
+import { styles } from "./HomeMainCarousel.styles";
+import CarrouselScrollIndicator from "../../../../modules/Home/component/CarouselScrollIndicator";
+import CarouselPaginationItem from "../../../../components/CarouselPaginationItem";
+import { useIsTester } from "../../../../hooks/useIsTester";
+import { useRemoteConfig } from "../../../../hooks/useRemoteConfig";
 
 interface IHomeMainCarousel {
   data: HomeCarouselOutput;
@@ -30,35 +31,44 @@ function HomeMainCarousel({ data }: IHomeMainCarousel) {
   const [currIndex, setCurrIndex] = useState(0);
   const $carousel = useRef<ICarouselInstance>();
 
-  const slideDelay = useMemo(() => (data.showtime || 10) * 1000, [data.showtime]);
+  const slideDelay = useMemo(
+    () => (data.showtime || 10) * 1000,
+    [data.showtime]
+  );
 
-  const showNewBullets = useMemo(() => (
-    getBoolean(isTester ? 'show_new_home_tester' : 'show_new_home')
-  ), [getBoolean, isTester]);
+  const showNewHome = useMemo(
+    () => getBoolean(isTester ? "show_new_home_tester" : "show_new_home"),
+    [getBoolean, isTester]
+  );
 
   const carouselHeight = useMemo(() => {
     const [item] = data.items;
 
     const { width, height } = item!.image;
 
-    return (width && height) ? ((height * configDeviceSizes.DEVICE_WIDTH) / width) : 500;
+    return width && height
+      ? (height * configDeviceSizes.DEVICE_WIDTH) / width
+      : 500;
   }, [data.items]);
 
-  const onPress = useCallback((item: HomeCarouselItemOutput) => {
-    const { reservaMini, orderBy } = item;
+  const onPress = useCallback(
+    (item: HomeCarouselItemOutput) => {
+      const { reservaMini, orderBy } = item;
 
-    return navigation.navigate('ProductCatalog', {
-      facetInput: item.facets,
-      referenceId: item.reference,
-      reservaMini,
-      orderBy,
-    });
-  }, [navigation]);
+      return navigation.navigate("ProductCatalog", {
+        facetInput: item.facets,
+        referenceId: item.reference,
+        reservaMini,
+        orderBy,
+      });
+    },
+    [navigation]
+  );
 
   return (
-    <View style={{ flex: 1 }} {...testProps('default_carrousel_container')}>
+    <View style={{ flex: 1 }} {...testProps("default_carrousel_container")}>
       <Carousel
-        {...testProps('default_carrousel_content')}
+        {...testProps("default_carrousel_content")}
         loop
         width={configDeviceSizes.DEVICE_WIDTH}
         height={carouselHeight}
@@ -76,18 +86,28 @@ function HomeMainCarousel({ data }: IHomeMainCarousel) {
         }}
         panGestureHandlerProps={{ activeOffsetX: [-10, 10] }}
         data={data.items}
-        style={{ backgroundColor: 'rgba(0, 0, 0, 0)', position: 'relative' }}
+        style={{ backgroundColor: "rgba(0, 0, 0, 0)", position: "relative" }}
         onSnapToItem={setCurrIndex}
         renderItem={({ item }) => (
-          <Box alignItems="flex-start">
+          <Box alignItems="flex-start" bg="white">
             <Box mb="quarck" width={1}>
               <Pressable
-                {...testProps('carrousel_button')}
+                {...testProps("carrousel_button")}
                 onPress={() => onPress(item)}
                 delayLongPress={100}
               >
                 <ImageComponent
-                  style={{ height: carouselHeight }}
+                  style={[
+                    {
+                      height: carouselHeight,
+                    },
+                    showNewHome
+                      ? {
+                          borderBottomLeftRadius: 8,
+                          borderBottomRightRadius: 8,
+                        }
+                      : null,
+                  ]}
                   resizeMode="cover"
                   source={{ uri: item.image.url }}
                 />
@@ -97,13 +117,16 @@ function HomeMainCarousel({ data }: IHomeMainCarousel) {
         )}
       />
 
-      {showNewBullets ? (
-        <View style={[styles.bulletsWrapper, { width: data.items.length * 17.5 }]}>
+      {showNewHome ? (
+        <View
+          style={[styles.bulletsWrapper, { width: data.items.length * 24.5 }]}
+        >
           {data.items.map((item, i) => (
             <CarouselPaginationItem
-              backgroundColor={COLORS.PRIMARY}
+              backgroundColor={COLORS.WHITE}
               animValue={progressValue}
               index={i}
+              actualPosition={currIndex}
               key={`home-main-carousel-${item.image.url}`}
               length={data.items.length}
             />
