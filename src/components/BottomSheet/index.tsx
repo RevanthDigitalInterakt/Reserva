7f28b77dcddfdafcef3c780fcc5f13b3b165cea7
Henrique Pires <henrique.p.espindola@gmail.com>
2023-10-17 09:54:02 -0300

    refactor: styles from correct file

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -2,36 +2,21 @@ import React from 'react';
 import { View, TouchableWithoutFeedback } from 'react-native';
 import Modal from 'react-native-modal';
 import type { BottomSheetProps } from './types';
+import styles from './styles';
 
 export function BottomSheet({ children, isOpen = false, onBackdropPress } : BottomSheetProps) {
   return (
     <View>
       <Modal
         isVisible={isOpen}
-        style={{
-          margin: 0,
-          justifyContent: 'flex-end',
-          flex: 1,
-        }}
+        style={styles.modal}
         customBackdrop={(
           <TouchableWithoutFeedback onPress={onBackdropPress}>
-            <View style={{
-              flex: 1,
-              backgroundColor: '#000000BF',
-            }}
-            />
+            <View style={styles.backdrop} />
           </TouchableWithoutFeedback>
           )}
       >
-        <View style={{
-          height: '50%',
-          backgroundColor: 'white',
-          borderTopEndRadius: 12,
-          borderTopStartRadius: 12,
-          paddingHorizontal: 24,
-          paddingTop: 24,
-        }}
-        >
+        <View style={styles.contentWrapper}>
           {children}
         </View>
       </Modal>
