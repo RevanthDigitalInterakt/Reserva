4bf58d3601b30952c85b085181b1bbe2e360933b
Gabriel Mazurco (RaktaTech) <gabriel.ribeiro@globalsys.com.br>
2023-11-17 18:22:06 -0300

    refactor(utils): removed unused utils func

--- src/utils/asyncRequestHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { AxiosResponse } from 'axios';
-
-export const asyncRequestHandler = async (promise: Promise<AxiosResponse>) => {
-  try {
-    return await promise;
-  } catch ({ response }: any) {
-    return response;
-  }
-};
