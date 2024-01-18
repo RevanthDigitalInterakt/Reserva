8ca3bcd33cad12aef17164038060ea9ba935df1e
Leonardo Henrique Nogueira <nogueirahy@gmail.com>
2024-01-18 14:27:01 -0300

    chore: add script to increment version in package.json

---
+++ scripts/updateVersion.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
+
+const BUILD_GRADLE_PATH = path.join(__dirname, '../android/app/build.gradle');
+const PACKAGE_JSON_PATH = path.join(__dirname, '../package.json');
+
+fs.readFile(BUILD_GRADLE_PATH, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Erro ao ler o build.gradle:', err);
+    process.exit(1);
+  }
+
+  const versionMatch = data.match(/versionName\s+"([^"]+)"/);
+  if (!versionMatch) {
+    console.error('Versão não encontrada no build.gradle');
+    process.exit(1);
+  }
+
+  const version = versionMatch[1];
+
+  fs.readFile(PACKAGE_JSON_PATH, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Erro ao ler o package.json:', err);
+      process.exit(1);
+    }
+
+    const packageJson = JSON.parse(data);
+    packageJson.version = version;
+
+    fs.writeFile(PACKAGE_JSON_PATH, JSON.stringify(packageJson, null, 2), 'utf8', (err) => {
+      if (err) {
+        console.error('Erro ao escrever no package.json:', err);
+        process.exit(1);
+      }
+
+      console.log(`Versão atualizada para ${version} no package.json`);
+    });
+  });
+});
