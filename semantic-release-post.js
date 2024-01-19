f04d40583612154571c64d0c445531b9ee94e01f
faria.guilherme <faria.guilherme@esx.com.br>
2024-01-19 21:05:41 +0000

    Updated semantic-release-post.js

--- semantic-release-post.js
+++ semantic-release-post.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['main', 'feat/onesignal-updated-v3'],
+  branches: ['main', 'feat/onesignal-updated-v3', 'feat/final-branch'],
   plugins: [
     ['@semantic-release/git', {
       // eslint-disable-next-line no-template-curly-in-string
