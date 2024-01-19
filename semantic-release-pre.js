37d0ad0edff238567e87ff84c16f2e018b73a8a4
Leonardo Henrique Nogueira <nogueirahy@gmail.com>
2024-01-19 16:41:50 -0300

    chore: ajust order

--- semantic-release-pre.js
+++ semantic-release-pre.js
@@ -1,12 +1,12 @@
 module.exports = {
   branches: ['main', 'feat/onesignal-updated-v3'],
   plugins: [
+    ['@semantic-release/exec', {
+      publishCmd: 'echo "ignore publish."',
+    }],
     'semantic-release-react-native',
     ['@semantic-release/npm', {
       npmPublish: false,
-    }],
-    ['@semantic-release/exec', {
-      publishCmd: 'echo "ignore publish."',
     }],
   ],
 };
