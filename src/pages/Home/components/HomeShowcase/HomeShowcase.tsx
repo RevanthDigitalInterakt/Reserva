9ce164e106f5ec637b5d0392f9bb471ca0045f14
Thulio Gamarano <thulio.gamarano@globalsys.com.br>
2024-02-28 10:06:33 -0300

    ajuste homeshowcase

--- src/pages/Home/components/HomeShowcase/HomeShowcase.tsx
+++ src/pages/Home/components/HomeShowcase/HomeShowcase.tsx
@@ -35,67 +35,28 @@ export interface IRsvProduct {
   productLink: string;
   brand: string;
   image: string;
-  name: string;
-  price: number;
+  categoryTree: string[];
+  flags: IRsvFlag[];
+  sku: IRsvSku[];
+  prices: IRsvPrice;
+}
+
+export interface IRsvRecommendation {
+  shelfName: string;
+  products: IRsvProduct[];
 }
 
 export function HomeShowcase() {
-  const data: IData[] = [
-    {
-      id: '1',
-      image: 'https://lojausereserva.vtexassets.com/arquivos/ids/8409628-400-600',
-      name: 'Camisa Reserva Linho',
-      price: 84,
-    },
-    {
-      id: '2',
-      image: 'https://lojausereserva.vtexassets.com/arquivos/ids/8409628-400-600',
-      name: 'Camisa Reserva Linho',
-      price: 84,
-    },
-    {
-      id: '3',
-      image: 'https://lojausereserva.vtexassets.com/arquivos/ids/8409628-400-600',
-      name: 'Camisa Reserva Linho',
-      price: 84,
-    },
-    {
-      id: '4',
-      image: 'https://lojausereserva.vtexassets.com/arquivos/ids/8409628-400-600',
-      name: 'Camisa Reserva Linho',
-      price: 84,
-    },
-    {
-      id: '5',
-      image: 'https://lojausereserva.vtexassets.com/arquivos/ids/8409628-400-600',
-      name: 'Camisa Reserva Linho',
-      price: 84,
-    },
-    {
-      id: '6',
-      image: 'https://lojausereserva.vtexassets.com/arquivos/ids/8409628-400-600',
-      name: 'Camisa Reserva Linho',
-      price: 84,
-    },
-  ];
+  const { onSearchShelf } = useRecommendationShelf() as IRecommendationShelfState;
+  const [shelf, setShelf] = useState<IRsvRecommendation[]>([]);
 
-  const renderItem = (item: IData) => (
-    <TouchableOpacity
-      style={{ padding: 10 }}
-      onPress={
-        () => trackClickSmartHintStore.getState().onSendTrackClick(item.id, TrackPageTypeEnum.Home)
-      }
-    >
-      <Image
-        source={{ uri: item.image }}
-        style={{
-          width: 150,
-          height: 200,
-        }}
-      />
-      <Text>{item.name}</Text>
-    </TouchableOpacity>
-  );
+  useEffect(() => {
+    async function handleGetShelf() {
+      const data = await onSearchShelf('home');
+      setShelf(data as IRsvRecommendation[]);
+    }
+    handleGetShelf();
+  }, [onSearchShelf]);
 
   return (
     <View style={styles.container}>
