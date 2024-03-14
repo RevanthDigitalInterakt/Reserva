1e547cdedf9d7a3f844e9fbd300b996df9854a70
Henrique Pires Espindola <henrique.p.espindola@gmail.com>
2024-03-14 14:44:03 -0300

    Refactor ProductPayment component to use Zustand and RemoteConfig

--- src/pages/ProductDetail/components/ProductPayment/index.tsx
+++ src/pages/ProductDetail/components/ProductPayment/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 import { Box } from '../../../../components/Box/Box';
 import { Button } from '../../../../components/Button';
@@ -6,30 +6,74 @@ import { IconLegacy } from '../../../../components/IconLegacy/IconLegacy';
 import { Typography } from '../../../../components/Typography/Typography';
 import testProps from '../../../../utils/testProps';
 import { ProductPaymentDescription } from './components/ProductPaymentDescription';
+import { useProductDetailStore } from '../../../../zustand/useProductDetail/useProductDetail';
+import { useRemoteConfig } from '../../../../hooks/useRemoteConfig';
+
+enum PaymentWaysEnum {
+  CREDIT_CARD = 'creditCardPaymentGroup',
+  GIFT_CARD = 'giftCardPaymentGroup',
+  TICKET = 'PagalevePaymentGroup',
+  NU_PAY = 'NubankPaymentGroup',
+  PIX = 'PagalevePixAVistaTransparentePaymentGroup',
+}
 
 function PaymentWays() {
+  const { productDetail } = useProductDetailStore(['productDetail']);
+  const { getString } = useRemoteConfig();
+  const creditCardDescription = getString('creditCardPaymentDescription');
+  const giftCardDescription = getString('giftCardPaymentDescription');
+  const nubankDescription = getString('nubankPaymentDescription');
+  const pixDescription = getString('pixPaymentDescription');
+  const hasCreditCardPaymentWay = productDetail?.paymentSystemGroupName?.includes(
+    PaymentWaysEnum.CREDIT_CARD,
+  );
+  const hasGiftCardPaymentWay = productDetail?.paymentSystemGroupName?.includes(
+    PaymentWaysEnum.GIFT_CARD,
+  );
+  const hasNuPayPaymentWay = productDetail?.paymentSystemGroupName?.includes(
+    PaymentWaysEnum.NU_PAY,
+  );
+  const hasPixPaymentWay = productDetail?.paymentSystemGroupName?.includes(
+    PaymentWaysEnum.PIX,
+  );
+
   return (
     <>
-      <ProductPaymentDescription title="Cartão de crédito" description="10x sem juros no cartão, comparcela mínima de R$ 60" testID="credit_card_payment_way" />
-      <ProductPaymentDescription title="Pix" description="Pague à vista ou até em 4x sem juros. " testID="pix_payment_way" />
-      <ProductPaymentDescription title="Nubank" description="Em até 24x" testID="nubank_payment_way" />
-      <ProductPaymentDescription title="Cartão Presente" description="Cartões de Presente Pré-pagos" testID="gift_card_payment_way" />
+      {hasCreditCardPaymentWay && (
+        <ProductPaymentDescription
+          title="Cartão de crédito"
+          description={creditCardDescription}
+          testID="credit_card_payment_way"
+        />
+      )}
+      {hasPixPaymentWay && (
+        <ProductPaymentDescription
+          title="Pix"
+          description={pixDescription}
+          testID="pix_payment_way"
+        />
+      )}
+      {hasNuPayPaymentWay && (
+        <ProductPaymentDescription
+          title="Nubank"
+          description={nubankDescription}
+          testID="nubank_payment_way"
+        />
+      )}
+      {hasGiftCardPaymentWay && (
+        <ProductPaymentDescription
+          title="Cartão Presente"
+          description={giftCardDescription}
+          testID="gift_card_payment_way"
+        />
+      )}
     </>
   );
 }
 
 export function ProductPayment() {
   const [showSection, setShowSection] = useState(false);
-  const onToggle = useCallback((show: boolean) => {
-    setShowSection(show);
-
-    // EventProvider.logEvent('product_view_about', {
-    //   show: show ? 1 : 0,
-    //   product_id: productDetail?.productId || '',
-    // });
-  }, []);
-  // }, [productDetail]);
-
+  const onToggle = (show: boolean) => setShowSection(show);
   return (
     <Box>
       <Button
@@ -39,16 +83,15 @@ export function ProductPayment() {
         flexDirection="row"
       >
         <>
-          {showSection
-            ? (
-              <Box alignSelf="center" paddingRight="quarck" paddingLeft="quarck">
-                <IconLegacy name="Subtraction" color="fullBlack" size={20} />
-              </Box>
-            ) : (
-              <Box alignSelf="center" paddingRight="nano">
-                <IconLegacy name="Add" color="fullBlack" size={20} />
-              </Box>
-            )}
+          {showSection ? (
+            <Box alignSelf="center" paddingRight="quarck" paddingLeft="quarck">
+              <IconLegacy name="Subtraction" color="fullBlack" size={20} />
+            </Box>
+          ) : (
+            <Box alignSelf="center" paddingRight="nano">
+              <IconLegacy name="Add" color="fullBlack" size={20} />
+            </Box>
+          )}
 
           <Box flex={1}>
             <Typography fontFamily="reservaSerifRegular" fontSize={20}>
@@ -59,7 +102,6 @@ export function ProductPayment() {
       </Button>
 
       {showSection && <PaymentWays />}
-
     </Box>
   );
 }
