4134f8bf0626e9dba65298797c8b0aa8d7296142
Felipe <felipe.nogueira@maeztra.com>
2024-05-03 18:31:21 +0000

    Merged PR 3953: 32093 - Abertura do link do whats com produtos pré definidos no app
    #### 🔨 O que foi realizado
    
    Ajustes no deep link que são enviados para os clientes via Whatsapp, para abrir a bag dentro do app e não redirecionar ao navegador, como estava acontecendo anteriormente.
    
    #### 🚨 Atrelou a história/bug ao PR?
    
    Sim
    
    #### 👾 Ambiente de testes
    
    Android Studio
    
    Related work items: #32093

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 
 import 'react-native-get-random-values';
 import { AppRegistry } from 'react-native';
+import 'react-native-url-polyfill/auto';
 import App from './src/App';
 import { name as appName } from './app.json';
 import onBackgroundEventPush from './src/utils/Notifee/BackgroundEvents';
-
 
 // if (__DEV__) {
 // NativeModules.DevSettings.setIsDebuggingRemotely(false);
