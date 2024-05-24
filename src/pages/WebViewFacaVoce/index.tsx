af5c24154e4d9d4300455abfa26f76526b85f761
Jean Marcos <jeanmarcos@frwk.com.br>
2024-05-24 10:24:21 -0300

    feat: cache was removed and the return icon was changed to close within the webview

--- src/pages/WebViewFacaVoce/index.tsx
+++ src/pages/WebViewFacaVoce/index.tsx
@@ -4,10 +4,10 @@ import WebView, { WebViewMessageEvent } from 'react-native-webview';
 import { Platform, View } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import { useBagStore } from '../../zustand/useBagStore/useBagStore';
-import { TopBarBackButton } from '../../modules/Menu/components/TopBarBackButton';
 import { trackPageViewStore } from '../../zustand/useTrackPageViewStore/useTrackPageViewStore';
 import testProps from '../../utils/testProps';
 import { useAuthStore } from '../../zustand/useAuth/useAuthStore';
+import { TopBarMenu } from '../../modules/Menu/components/TopBarMenu';
 
 interface IOnLoad {
   nativeEvent: any;
@@ -33,7 +33,7 @@ export default function WebViewFacaVoce() {
     const { event: type } = JSON.parse(data);
 
     switch (type) {
-      case 'facavccarrinho': {
+      case 'facavcgotocart': {
         navigation.navigate('BagScreen');
         return null;
       }
@@ -46,11 +46,7 @@ export default function WebViewFacaVoce() {
   return (
     <>
       <View>
-        <TopBarBackButton
-          showShadow
-          backButtonPress={() => navigation.goBack()}
-          loading={loading}
-        />
+        <TopBarMenu loading={loading} />
       </View>
       <WebView
         {...testProps('web_view_facavc')}
@@ -59,6 +55,7 @@ export default function WebViewFacaVoce() {
         cacheMode="LOAD_NO_CACHE"
         clearCache
         startInLoadingState
+        saveFormDataDisabled
         onLoadStart={(event: IOnLoad) => {
           const { nativeEvent } = event;
           setLoading(nativeEvent.loading);
