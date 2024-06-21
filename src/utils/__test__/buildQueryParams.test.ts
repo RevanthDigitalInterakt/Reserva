56bdc36bc7e5ae13d1147f39dc59c0028165143a
Jean Marcos <jeanmarcos@frwk.com.br>
2024-06-21 16:46:10 -0300

    feat: improve function to handle array, object, among others | also added cast to string in the sanitize function

--- src/utils/__test__/buildQueryParams.test.ts
+++ src/utils/__test__/buildQueryParams.test.ts
@@ -18,4 +18,52 @@ describe('buildQueryParams', () => {
     const result = buildQueryParams(params);
     expect(result).toBe('name=John&age=30&address=test_mock_teste_address');
   });
+
+  it('should handle numeric values correctly', () => {
+    const params = { count: 123, price: 19.99 };
+    const result = buildQueryParams(params);
+    expect(result).toBe('count=123&price=1999');
+  });
+
+  it('should handle boolean values correctly', () => {
+    const params = { isActive: true, isAdmin: false };
+    const result = buildQueryParams(params);
+    expect(result).toBe('isActive=true&isAdmin=false');
+  });
+
+  it('should handle null and undefined values correctly', () => {
+    const params = { name: 'John', age: null, gender: undefined };
+    const result = buildQueryParams(params);
+    expect(result).toBe('name=John&age=null&gender=undefined');
+  });
+
+  it('should handle arrays correctly', () => {
+    const params = { items: ['item1', 'item2', 'item3'] };
+    const result = buildQueryParams(params);
+    expect(result).toBe('items=item1&items=item2&items=item3');
+  });
+
+  it('should encode non-ASCII characters correctly', () => {
+    const params = { name: 'Jöhn', city: 'München' };
+    const result = buildQueryParams(params);
+    expect(result).toBe('name=Jhn&city=Mnchen');
+  });
+
+  it('should handle empty string values correctly', () => {
+    const params = { name: '', age: '30' };
+    const result = buildQueryParams(params);
+    expect(result).toBe('name=&age=30');
+  });
+
+  it('should handle nested objects correctly', () => {
+    const params = { details: { address: '123 Main St', city: 'Anytown' } };
+    const result = buildQueryParams(params);
+    expect(result).toBe('details-address=123_Main_St&details-city=Anytown');
+  });
+
+  it('should handle nested objects correctly case 2', () => {
+    const params = { details: { address: { street: 'Main St', number: 123 }, city: 'Anytown' } };
+    const result = buildQueryParams(params);
+    expect(result).toBe('details-address-street=Main_St&details-address-number=123&details-city=Anytown');
+  });
 });
--- src/utils/sanitizeString.ts
+++ src/utils/sanitizeString.ts
@@ -1,3 +1,3 @@
-export const sanitizeString = (str: string): string => str
+export const sanitizeString = (str: string): string => String(str)
   .replace(/ /g, '_')
   .replace(/[^a-zA-Z0-9_-]/g, '');
