4e310ba1973cb76e301597e3acef2a1bacee3fb2
Leonardo Henrique Nogueira <nogueirahy@gmail.com>
2024-09-16 17:31:47 -0300

    fix: update commit analyser

--- semantic-release-pre.js
+++ semantic-release-pre.js
@@ -1,6 +1,12 @@
 module.exports = {
   branches: ['feat/refact-pipeline'],
   plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "conventionalcommits",
+      }
+    ],
     'semantic-release-react-native',
     ['@semantic-release/npm', {
       npmPublish: false,
