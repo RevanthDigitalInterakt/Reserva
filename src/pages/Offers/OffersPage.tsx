7e0af87a7e5e157609a9b8e2f816bcce80edb24e
Henrique Pires Espindola <henrique.p.espindola@gmail.com>
2024-09-27 07:47:39 -0300

    feat: integrate NewProductCatalog into OffersPage with navigation props

--- src/pages/Offers/OffersPage.tsx
+++ src/pages/Offers/OffersPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, View } from 'react-native';
+import type { StackScreenProps } from '@react-navigation/stack';
 import { OffersCarousels } from './Components/OffersCarousels/OffersCarousels';
 import { TopBarDefault } from '../../modules/Menu/components/TopBarDefault';
 import CategoryComponent from './Components/CategoryComponent/CategoryComponent';
+import NewProductCatalog from '../ProductCatalog';
+import type { RootStackParamList } from '../../routes/StackNavigator';
 
-function OffersPage() {
+type Props = StackScreenProps<RootStackParamList, 'ProductCatalog'>;
+
+function OffersPage({ navigation, route }: Props) {
   return (
     <View style={{ flex: 1 }}>
       <TopBarDefault showShadow />
@@ -12,6 +17,12 @@ function OffersPage() {
         <ScrollView>
           <OffersCarousels />
           <CategoryComponent />
+          <NewProductCatalog
+            navigation={navigation}
+            route={route}
+            showTabBar={false}
+            showWhatsappButton={false}
+          />
         </ScrollView>
       </SafeAreaView>
     </View>
