38a9fe78a8187c3aa66f80976fd33d7895a9b0b4
Cleiber Reis <cleiber.reis@usereserva.com>
2025-01-09 15:33:04 -0300

    fix: remove .jso folder and update Podfile.lock

--- .jso/src/src/routes/flows/login.flow.js
+++
@@ -1,9 +0,0 @@
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-  exports.LoginFlow = undefined;
-  var _LoginABTest = _$$_REQUIRE(_dependencyMap[0])(_$$_REQUIRE(_dependencyMap[1]));
-  var LoginFlow = exports.LoginFlow = [{
-    component: _LoginABTest.default,
-    name: 'Login'
-  }];
--- .jso/src/src/utils/Dito/src/utils/getDitoUserID.js
+++
@@ -1,8 +0,0 @@
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-  exports.getDitoUserID = getDitoUserID;
-  var _asyncStorage = _$$_REQUIRE(_dependencyMap[0])(_$$_REQUIRE(_dependencyMap[1]));
-  function getDitoUserID(email) {
-    return email ? (0, _$$_REQUIRE(_dependencyMap[2]).getAsyncStorageItem)('@Dito:userRef') : _asyncStorage.default.getItem('@Dito:anonymousID');
-  }
